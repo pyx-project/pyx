f714272f83e0a4340f44b48a81ed2a3c839b8bd7
Joerg Lehmann <joerg@pyx-project.org>
2001-08-09 21:43:24 +0000

    -some corrections -test suite added

--- pyx/trafo.py
+++ pyx/trafo.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+# TODO: 
+# - switch to affine space description (i.e. represent transformation by
+#   3x3 matrix (cf. PLRM Sect. 4.3.3)? Cooler!
+#
+
 # some helper routines
 
 def _rmatrix(angle):
@@ -16,11 +21,22 @@ def _mmatrix(angle):
     return ( (cos(phi)*cos(phi)-sin(phi)*sin(phi), -2*sin(phi)*cos(phi)                ),
 	     (-2*sin(phi)*cos(phi),                sin(phi)*sin(phi)-cos(phi)*cos(phi) ) )
 
+def _det(matrix):
+    return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+
+# Exception
+
+class UndefinedResultError(ArithmeticError):
+    pass
+
 # trafo: affine transformations
 	     
 class trafo:
     def __init__(self, matrix=((1,0),(0,1)), vector=(0,0)):
-        self.matrix=matrix
+        if _det(matrix)==0:		
+	    raise UndefinedResultError, "trafo matrix must not be singular" 
+	else:
+            self.matrix=matrix
         self.vector=vector
 
     def __mul__(self, other):
@@ -60,12 +76,17 @@ class trafo:
     def mirror(self,angle):
 	return trafo(matrix=_mmatrix(angle))*self
 
+    def scale(self, x, y):
+        return trafo(matrix=((x, 0), (0,y)))*self
+
     def inverse(self):
-        det = self.matrix[0][0]*self.matrix[1][1] - self.matrix[0][1]*self.matrix[1][0]
-	# TODO: Better error handling, or is it ok to through ZeroDivisionError for singular matrices?
-        matrix = ( ( self.matrix[1][1]/det, -self.matrix[0][1]/det),
-	           (-self.matrix[1][0]/det,  self.matrix[0][0]/det)
-		 )
+        det = _det(self.matrix)				# shouldn't be zero, but
+	try: 
+          matrix = ( ( self.matrix[1][1]/det, -self.matrix[0][1]/det),
+	             (-self.matrix[1][0]/det,  self.matrix[0][0]/det)
+	 	   )
+        except ZeroDivisionError:
+	   raise UndefinedResultError, "trafo matrix must not be singular" 
         return trafo(matrix=matrix) * trafo(vector=(-self.vector[0],-self.vector[1]))
 	
     def __repr__(self):
@@ -82,42 +103,44 @@ class rotate(trafo):
 class mirror(trafo):
     def __init__(self,angle=0):
         trafo.__init__(self, matrix=_mmatrix(angle))
+
+class scale(trafo):
+    def __init__(self,x,y):
+        trafo.__init__(self, matrix=((x,0),(0,y)))
         
 
 if __name__=="__main__":
-   # some invariants:
-   #  trafo(angle=angle, vector=(x,y)) == translate(x,y) * rotate(angle)
-   from math import pi, cos, sin
-   angle = 15
-   phi = 2*pi*angle/360
-   matrix=((cos(phi), sin(phi)), (-sin(phi), cos(phi)))
+   # test for some invariants:
 
-   print translate(1,3) * rotate(15) 
-   print trafo(matrix=matrix, vector=(1,3) )
-   print
+   def checkforidentity(trafo):
+       m = max(map(abs,[trafo.matrix[0][0]-1,
+                        trafo.matrix[1][0],
+                        trafo.matrix[0][1],
+                        trafo.matrix[1][1]-1,
+	                trafo.vector[0],
+	                trafo.vector[1]]))
+		    
+       assert m<1e-7, "tests for invariants failed" 
+	    
+
+   # trafo(angle=angle, vector=(x,y)) == translate(x,y) * rotate(angle)
+   checkforidentity( translate(1,3) * rotate(15)  * trafo(matrix=_rmatrix(15),vector=(1,3)).inverse())
    
    # t*t.inverse() == 1
    t = translate(-1,-1)*rotate(72)*translate(1,1)
-   print t*t.inverse()
-   print trafo()
-   print
+   checkforidentity(t*t.inverse())
 
    # -mirroring two times should yield identiy
    # -mirror(phi)=mirror(phi+180)
-
-   print mirror(20)*mirror(20)
-   print mirror(20).mirror(20)
-   print mirror(20)*mirror(180+20)
-   print trafo()
-   print
+   checkforidentity( mirror(20)*mirror(20)) 
+   checkforidentity( mirror(20).mirror(20)) 
+   checkforidentity( mirror(20)*mirror(180+20))
    
    # equivalent notations
-   print translate(1,2).rotate(72).translate(-3,-1)
-   print translate(-3,-1)*rotate(72)*translate(1,2)
-   print
+   checkforidentity( translate(1,2).rotate(72).translate(-3,-1)*(translate(-3,-1)*rotate(72)*translate(1,2)).inverse() )
    
-   print rotate(90).rotate(90).rotate(90).rotate(90)
-   print trafo()
-   print
+   checkforidentity( rotate(40).rotate(120).rotate(90).rotate(110) )
+
+   checkforidentity( scale(2,3).scale(1/2.0, 1/3.0) )
    
   
