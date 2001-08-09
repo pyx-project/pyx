596e68d1befff221c578770c676fa6f58075959f
Joerg Lehmann <joerg@pyx-project.org>
2001-08-09 18:46:59 +0000

    back to matrix implementation => mirroring implemented

--- pyx/transformation.py
+++ pyx/transformation.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
 
+# affine transformations
+
 class transformation:
-    def __init__(self, angle=0, vector=(0,0)):
-        self.angle=angle
+    def __init__(self, matrix=((1,0),(0,1)), vector=(0,0)):
+        self.matrix=matrix
         self.vector=vector
 
     def __mul__(self, other):
         if isinstance(other, transformation):
-	    angle  = self.angle + other.angle
-            vector = ( self.matrix()[0][0]*other.vector[0] + self.matrix()[0][1]*other.vector[1] + self.vector[0],
-                       self.matrix()[1][0]*other.vector[0] + self.matrix()[1][1]*other.vector[1] + self.vector[1] )
+            matrix = ( ( self.matrix[0][0]*other.matrix[0][0] + self.matrix[0][1]*other.matrix[1][0],
+                         self.matrix[0][0]*other.matrix[0][1] + self.matrix[0][1]*other.matrix[1][1] ),
+                       ( self.matrix[1][0]*other.matrix[0][0] + self.matrix[1][1]*other.matrix[1][0],
+                         self.matrix[1][0]*other.matrix[0][1] + self.matrix[1][1]*other.matrix[1][1] )
+                     )
+            vector = ( self.matrix[0][0]*other.vector[0] + self.matrix[0][1]*other.vector[1] + self.vector[0],
+                       self.matrix[1][0]*other.vector[0] + self.matrix[1][1]*other.vector[1] + self.vector[1] )
 
             # print " ( %s * %s => %s ) "% (self, other, transformation(angle=angle, vector=vector))
 		      
-	    return transformation(angle=angle, vector=vector)
+	    return transformation(matrix=matrix, vector=vector)
 	else:
 	    raise NotImplementedError, "can only multiply two transformations"
-    def __rmul__(self, other):
+    def __rmul__(self, other):				# TODO: not needed!?
         print "!"
         return other.__mul__(self)
 
-    def matrix(self):
-        from math import pi, cos, sin
-	phi = 2*pi*self.angle/360
-        return ( ( cos(phi), sin(phi)), 
-	         (-sin(phi), cos(phi)) )
+    def matrix():
+        return self.matrix
 
     def vector(self):
         return self.vector
@@ -36,10 +39,24 @@ class transformation:
 	return transformation(vector=(x,y))*self
 	
     def rotate(self,angle):
-	return transformation(angle=angle)*self
+        from math import pi, cos, sin
+	phi = 2*pi*angle/360
+	
+	matrix = (( cos(phi), sin(phi)), 
+	          (-sin(phi), cos(phi)))
+		  
+	return transformation(matrix=matrix)*self
+
+    def inverse(self):
+        det = self.matrix[0][0]*self.matrix[1][1] - self.matrix[0][1]*self.matrix[1][0]
+	# TODO: Better error handling, or is it ok to through ZeroDivisionError for singular matrices?
+        matrix = ( ( self.matrix[1][1]/det, -self.matrix[0][1]/det),
+	           (-self.matrix[1][0]/det,  self.matrix[0][0]/det)
+		 )
+        return transformation(matrix=matrix) * transformation(vector=(-self.vector[0],-self.vector[1]))
 	
     def __repr__(self):
-        return "angle=%f, vector=%s" % (self.angle, self.vector)
+        return "matrix=%s, vector=%s" % (self.matrix, self.vector)
 
 class translate(transformation):
     def __init__(self,x,y):
@@ -47,16 +64,58 @@ class translate(transformation):
    
 class rotate(transformation):
     def __init__(self,angle):
-        transformation.__init__(self, angle=angle)
+        from math import pi, cos, sin
+	phi = 2*pi*angle/360
+	
+	matrix = (( cos(phi), sin(phi)), 
+	          (-sin(phi), cos(phi)) )
+		  
+        transformation.__init__(self, matrix=matrix)
+	
+class mirror(transformation):
+    def __init__(self,angle=0):
+        from math import pi, cos, sin
+	phi = 2*pi*angle/360
+
+	matrix=( (cos(phi)*cos(phi)-sin(phi)*sin(phi), -2*sin(phi)*cos(phi)                ),
+	         (-2*sin(phi)*cos(phi),                sin(phi)*sin(phi)-cos(phi)*cos(phi) ) )
+       
+        transformation.__init__(self, matrix=matrix)
+        
 
 if __name__=="__main__":
-#   print rotate(90).rotate(90).rotate(90).rotate(90)
-   print "::", translate(1,1).rotate(72).translate(-1,-1)
-   print "::", translate(-1,-1)*rotate(72)*translate(1,1)
-#   print translate(-1,-1).rotate(90).translate(1,1)
+   # some invariants:
+   #  transformation(angle=angle, vector=(x,y)) == translate(x,y) * rotate(angle)
+   from math import pi, cos, sin
+   angle = 15
+   phi = 2*pi*angle/360
+   matrix=((cos(phi), sin(phi)), (-sin(phi), cos(phi)))
+
+   print translate(1,3) * rotate(15) 
+   print transformation(matrix=matrix, vector=(1,3) )
+   print
+   
+   # t*t.inverse() == 1
+   t = translate(-1,-1)*rotate(72)*translate(1,1)
+   print t*t.inverse()
+   print transformation()
+   print
+
+   # -mirroring two times should yield identiy
+   # -mirror(phi)=mirror(phi+180)
+
+   print mirror(20)*mirror(20)
+   print mirror(20)*mirror(180+20)
+   print transformation()
+   print
+   
+   # equivalent notations
+   print translate(1,2).rotate(72).translate(-3,-1)
+   print translate(-3,-1)*rotate(72)*translate(1,2)
+   print
+   
+   print rotate(90).rotate(90).rotate(90).rotate(90)
+   print transformation()
+   print
+   
   
-# tmatrix = ( ( self.tmatrix[0][0]*other.tmatrix[0][0] + self.tmatrix[0][1]*other.tmatrix[1][0],
-#               self.tmatrix[0][0]*other.tmatrix[0][1] + self.tmatrix[0][1]*other.tmatrix[1][1] ),
-#             ( self.tmatrix[1][0]*other.tmatrix[0][0] + self.tmatrix[1][1]*other.tmatrix[1][0],
-#               self.tmatrix[1][0]*other.tmatrix[0][1] + self.tmatrix[1][1]*other.tmatrix[1][1] )
-#           )
