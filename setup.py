d88dd36738de882fa7217a3f63dc48213955b847
Joerg Lehmann <joerg@pyx-project.org>
2001-12-02 16:11:26 +0000

    - created initial version of distutil setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Pyx",
+      version="0.1",
+      description="Python package for the generation of mixed PS and (La)TeX code",
+      packages=['pyx'],
+     )
+
