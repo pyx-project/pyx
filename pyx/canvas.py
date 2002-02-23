cde1bf923ec0fc5f2a2182e18e01c2646c038c7a
Joerg Lehmann <joerg@pyx-project.org>
2002-02-23 16:43:13 +0000

    - minor cleanups - removed example code from canvas.py

--- pyx/canvas.py
+++ pyx/canvas.py
@@ -20,6 +20,9 @@
 # along with PyX; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+# TODO:
+# - arrows
+
 import string, re, tex, unit, trafo, types
 from math import sqrt
 
@@ -82,11 +85,9 @@ class bbox:
     def __add__(self, other):
         ' join two bboxes '
 
-
         return bbox(_nmin(self.llx, other.llx), _nmin(self.lly, other.lly),
                     max(self.urx, other.urx), max(self.ury, other.ury))
-
-#    __radd__=__add__
+    
 
     def __mul__(self, other):
         ' intersect two bboxes '
@@ -96,6 +97,7 @@ class bbox:
 
     def intersects(self, other):
         ' check, if two bboxes intersect eachother '
+        
         return not (self.llx > other.urx or
                     self.lly > other.ury or
                     self.urx < other.llx or
@@ -116,7 +118,9 @@ bbpattern = re.compile( r"^%%BoundingBox:\s+([+-]?\d+)\s+([+-]?\d+)\s+([+-]?\d+)
 
 class epsfile:
 
-    def __init__(self, filename, x = "0 t m", y = "0 t m", unit = unit.unit(), clip = 1, translatebb = 1, showbb = 0):
+    def __init__(self, filename, x = "0 t m", y = "0 t m",
+                 unit = unit.unit(),
+                 clip = 1, translatebb = 1, showbb = 0):
         self.unit        = unit
         self.x           = unit.pt(x)
         self.y           = unit.pt(y)
@@ -126,7 +130,8 @@ class epsfile:
         self.showbb      = showbb
 
     def getbbox(self, translatebb):
-        'determines bounding box of EPS file filename as 4-tuple (llx, lly, urx, ury)'
+        'returns bounding box of EPS file filename as 4-tuple (llx, lly, urx, ury)'
+        
         try:
             file = open(self.filename,"r")
         except:
@@ -149,7 +154,7 @@ class epsfile:
                    (llx, lly, urx, ury) = (0, 0, urx - llx, ury - lly)
 	       return bbox(llx, lly, urx, ury)
 
-    def bbox(self, canvas=None):
+    def bbox(self, canvas):
         return self.getbbox(self.translatebb)
 
     def write(self, canvas, file):
@@ -158,7 +163,7 @@ class epsfile:
         try:
 	    epsfile=open(self.filename,"r")
 	except:
-	    assert "cannot open EPS file"	                          # TODO: Fehlerbehandlung
+	    assert "cannot open EPS file"	               # TODO: Fehlerbehandlung
 
         file.write("BeginEPSF\n")
         file.write("%f %f translate\n" % (self.x, self.y))
@@ -444,111 +449,3 @@ class canvas(CanvasCmds):
         file.write("\nshowpage\n")
         file.write("%%Trailer\n")
         file.write("%%EOF\n")
-
-if __name__=="__main__":
-    from graph import *
-    from tex   import *
-    from path  import *
-    from trafo import *
-    from color import *
-    import unit
-
-    c=canvas.canvas(unit=unit.unit())
-    t=c.insert(tex())
- 
-    #for x in range(11):
-    #    amove(x,0)
-    #    rline(0,20)
-    #for y in range(21):
-    #   amove(0,y)
-    #   rline(10,0)
- 
-    c.draw(path( [moveto(1,1), 
-                  lineto(2,2), 
-                  moveto(1,2), 
-                  lineto(2,1) ] 
-        	)
-          )
-
-
-    c.draw(line(1, 1, 1,2)) 
- 
-    print "Breite von 'Hello world!': ",t.textwd("Hello  world!")
-    print "Höhe von 'Hello world!': ",t.textht("Hello world!")
-    print "Höhe von 'Hello world!' in large: ",t.textht("Hello world!", fontsize.large)
-    print "Höhe von 'Hello world!' in Large: ",t.textht("Hello world!", fontsize.Large)
-    print "Höhe von 'Hello world' in huge: ",t.textht("Hello world!", fontsize.huge)
-    print "Tiefe von 'Hello world!': ",t.textwd("Hello world!")
-    print "Tiefe von 'Hello world!': ",t.textht("Hello world!")
-    print "Tiefe von 'Hello world!': ",t.textdp("Hello world!")
-    print "Tiefe von 'was mit q': ",t.textdp("was mit q")
-    print "Tiefe von 'was mit q': ",t.textdp("was mit q")
-    t.text(5, 1, "Hello world!")
-    t.text(15, 1, "Hello world!", fontsize.huge)
-    t.text(5, 2, "Hello world!", halign.center)
-    t.text(5, 3, "Hello world!", halign.right)
-    for angle in (-90,-80,-70,-60,-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,80,90):
-        t.text(11+angle/10, 5, str(angle), direction(angle))
-        t.text(11+angle/10, 6, str(angle), direction(angle), halign.center)
-        t.text(11+angle/10, 7, str(angle), direction(angle), halign.right)
-    for pos in range(2,21):
-        c.draw(line(pos, 4.5, pos, 7.5)) 
-    c.draw(line(1.5, 5, 20.5, 5)) 
-    c.draw(line(1.5, 6, 20.5, 6)) 
-    c.draw(line(1.5, 7, 20.5, 7)) 
-   
-    p=path([ moveto(5,12), 
-             lineto(7,12), 
-             moveto(5,10), 
-             lineto(5,14), 
-             moveto(7,10), 
-             lineto(7,14)])
-   
-    c.set(canvas.linestyle.dotted)
-    t.text(5, 12, "a b c d e f g h i j k l m n o p q r s t u v w x y z", hsize("2 cm", c))
-    c.draw(p)
- 
-    p=path([ moveto(10,12), 
-             lineto(12,12), 
-             moveto(10,10), 
-             lineto(10,14), 
-             moveto(12,10), 
-             lineto(12,14)])
-    c.set(canvas.linestyle.dashdotted, rgb(1,0,0))
-    t.text("10 cm", 12, "a b c d e f g h i j k l m n o p q r s t u v w x y z", hsize("2 cm", c), valign.bottom, grey(0.5))
-    c.draw(p)
- 
-    p=path([moveto(5,15), arc(5,15, 1, 0, 45), closepath()])
-    c.fill(p, canvas.linestyle.dotted, canvas.linewidth.THICK)
- 
-    p=path([moveto(5,17), curveto(6,18, 5,16, 7,15)])
-    c.draw(p, canvas.linestyle.dashed)
-
-   
-    for angle in range(20):
-       s=c.insert(canvas.canvas(translate(10,10)*rotate(angle))).draw(p, canvas.linestyle.dashed, canvas.linewidth(0.01*angle), grey((20-angle)/20.0))
-
- 
-    c.set(linestyle.solid)
-    g=GraphXY(c, t, 10, 15, 8, 6, x=LogAxis())
-    df = DataFile("testdata")
-    g.plot(Data(df, x=1, y=3))
-    #g.plot(Data(df, x=2, y2=4))
-    #g.plot(Data(df, x=2, y=5), mark(0.01))
-    #g.plot(Data(df, x=2, y=6), chain())
-    #g.plot(Data(df, x=2, y=7))
-    #g.plot(Data(df, x=2, y=8))
-#    g.plot(Function("0.01*sin(x)",Points=1000))
-#    g.plot(Function("0", Points=2000)) # <- make this working!
-    #g.plot(Function("0*x"))
-    #g.plot(Function("0.01*sin(x)"), chain())
-    #g.plot(Function("x=2*sin(1000*y)"))
-    g.run()
-    
-#    c.insert(canvas.canvas(scale(0.5, 0.4).rotate(10).translate("2 cm","200 mm"))).insert(epsfile("ratchet_f.eps"))
-#    c.insert(canvas.canvas(scale(0.2, 0.1).rotate(10).translate("6 cm","180 mm"))).insert(epsfile("ratchet_f.eps"))
-    
-    c.draw(path([moveto("5 cm", "5 cm"), rlineto(0.1,0.1)]), linewidth.THICK)
-
-    c.writetofile("example")
-
