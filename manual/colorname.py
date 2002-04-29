eb366ebc87daf0cd528ccce5ab48f1f4e92b7ec1
Andre Wobst <wobsta@pyx-project.org>
2002-04-29 18:27:10 +0000

    *** empty log message ***

--- manual/colorname.py
+++ manual/colorname.py
@@ -7,24 +7,32 @@ from pyx import *
 c = canvas.canvas()
 t = tex.latex()
 t.define(r"\renewcommand{\familydefault}{\ttdefault}")
-# colorlist = filter(lambda (x, y): isinstance(y, color.cmyk),
-#                    color.cmyk.__dict__.items())
-# colorlist.sort(lambda x, y: cmp(x[1].sort, y[1].sort))
 p = re.compile("([a-z]+)\\.([a-z]+) += ([a-z]+)\\([0-9\\., ]+\\)\n", re.IGNORECASE)
 lines = imp.find_module("color", pyx.__path__)[0].readlines()
 x = 0
 y = 0
-dx = 5
-dy = 0.68
+dx = 4.8
+dy = -0.65
+oldg0 = 0
 for line in lines:
     m = p.match(line)
     if m:
         g = m.groups()
         if g[0] == g[2]:
-            c.fill(path.rect(x, y, 0.5, 0.5), pyx.color.__dict__[g[0]].__dict__[g[1]])
-            t.text(x+1, y, "%s.%s" % (g[0], g[1]))
+            if oldg0 and (g[0] != oldg0):
+                y += dy
+            myc = pyx.color.__dict__[g[0]].__dict__[g[1]]
+            c.draw(path.line(x + 0.1, y + 0.1, x + 0.4, y + 0.4), myc)
+            c.draw(path.line(x + 0.4, y + 0.1, x + 0.1, y + 0.4), myc)
+            c.fill(path.rect(x + 0.5, y, 1, 0.5), myc)
+            c.draw(path.line(x + 0.6, y + 0.1, x + 0.9, y + 0.4), color.grey.black)
+            c.draw(path.line(x + 0.9, y + 0.1, x + 0.6, y + 0.4), color.grey.black)
+            c.draw(path.line(x + 1.1, y + 0.1, x + 1.4, y + 0.4), color.grey.white)
+            c.draw(path.line(x + 1.4, y + 0.1, x + 1.1, y + 0.4), color.grey.white)
+            t.text(x + 1.7, y + 0.15, "%s.%s" % (g[0], g[1]), tex.fontsize.footnotesize)
             y += dy
-            if y > 16.5:
+            oldg0 = g[0]
+            if y < -16:
                 y = 0
                 x += dx
 
