f84ba0629a6b3a0d3076f97045e75bebe9dbfab5
Andre Wobst <wobsta@pyx-project.org>
2002-05-27 08:34:35 +0000

    *** empty log message ***

--- manual/colorname.py
+++ manual/colorname.py
@@ -7,34 +7,37 @@ from pyx import *
 c = canvas.canvas()
 t = tex.latex()
 t.define(r"\renewcommand{\familydefault}{\ttdefault}")
-p = re.compile("([a-z]+)\\.([a-z]+) += ([a-z]+)\\([0-9\\., ]+\\)\n", re.IGNORECASE)
-lines = imp.find_module("color", pyx.__path__)[0].readlines()
+
+# positioning is quite ugly ... but it works at the moment
 x = 0
 y = 0
 dx = 4.8
 dy = -0.65
-oldg0 = 0
-for line in lines:
+lastmodel = 0
+
+# we could use (grey|rgb|cmyk).__dict__ to get the instances, but we
+# would loose the ordering ... instead we just parse the file:
+p = re.compile("(?P<id>(?P<model>[a-z]+)\\.(?P<name>[a-z]+)) += (?P=model)\\([0-9\\., ]+\\)\n", re.IGNORECASE)
+lines = imp.find_module("color", pyx.__path__)[0].readlines()
+for line in lines: # we yet don't use a file iterator
     m = p.match(line)
     if m:
-        g = m.groups()
-        if g[0] == g[2]:
-            if oldg0 and (g[0] != oldg0):
-                y += dy
-            myc = pyx.color.__dict__[g[0]].__dict__[g[1]]
-            c.draw(path.line(x + 0.1, y + 0.1, x + 0.4, y + 0.4), myc)
-            c.draw(path.line(x + 0.4, y + 0.1, x + 0.1, y + 0.4), myc)
-            c.fill(path.rect(x + 0.5, y, 1, 0.5), myc)
-            c.draw(path.line(x + 0.6, y + 0.1, x + 0.9, y + 0.4), color.grey.black)
-            c.draw(path.line(x + 0.9, y + 0.1, x + 0.6, y + 0.4), color.grey.black)
-            c.draw(path.line(x + 1.1, y + 0.1, x + 1.4, y + 0.4), color.grey.white)
-            c.draw(path.line(x + 1.4, y + 0.1, x + 1.1, y + 0.4), color.grey.white)
-            t.text(x + 1.7, y + 0.15, "%s.%s" % (g[0], g[1]), tex.fontsize.footnotesize)
-            y += dy
-            oldg0 = g[0]
-            if y < -16:
-                y = 0
-                x += dx
+        if lastmodel and (m.group("model") != lastmodel):
+           y += dy
+        myc = pyx.color.__dict__[m.group("model")].__dict__[m.group("name")]
+        c.draw(path.line(x + 0.1, y + 0.1, x + 0.4, y + 0.4), myc)
+        c.draw(path.line(x + 0.4, y + 0.1, x + 0.1, y + 0.4), myc)
+        c.fill(path.rect(x + 0.5, y, 1, 0.5), myc)
+        c.draw(path.line(x + 0.6, y + 0.1, x + 0.9, y + 0.4), color.grey.black)
+        c.draw(path.line(x + 0.9, y + 0.1, x + 0.6, y + 0.4), color.grey.black)
+        c.draw(path.line(x + 1.1, y + 0.1, x + 1.4, y + 0.4), color.grey.white)
+        c.draw(path.line(x + 1.4, y + 0.1, x + 1.1, y + 0.4), color.grey.white)
+        t.text(x + 1.7, y + 0.15, m.group("id"), tex.fontsize.footnotesize)
+        y += dy
+        lastmodel = m.group("model")
+        if y < -16:
+            y = 0
+            x += dx
 
 c.insert(t)
 c.writetofile("colorname")
