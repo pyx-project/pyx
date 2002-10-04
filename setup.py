bf1b1417d2af63d74a78290879ebcfdfd287ad65
Joerg Lehmann <joerg@pyx-project.org>
2002-10-04 15:34:52 +0000

    added more metadata

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from distutils.core import setup
 
 setup(name="Pyx",
       version="0.1",
+      author="Jörg Lehmann, André Wobst",
+      author_email="pyx-devel@lists.sourceforge.net",
+      url="http://pyx.sourceforge.net/",
       description="Python package for the generation of mixed PS and (La)TeX code",
       packages=['pyx'],
      )
