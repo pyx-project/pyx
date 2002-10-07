47c70ce66679fe7d0bce798d6109f0ade511d1fe
Joerg Lehmann <joerg@pyx-project.org>
2002-10-07 12:13:38 +0000

    added MANIFEST.in

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name="PyX",
       author_email="pyx-devel@lists.sourceforge.net",
       url="http://pyx.sourceforge.net/",
       description="Python package for the generation of mixed PS and (La)TeX code",
-      packages=['pyx'],
+      license="GPL",
+      packages=['pyx']
      )
 
