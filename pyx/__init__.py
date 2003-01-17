e80266b17633b3a5de61b205267f5efbf95bb395
Joerg Lehmann <joerg@pyx-project.org>
2003-01-17 11:38:20 +0000

    bumped version to 0.2

--- pyx/__init__.py
+++ pyx/__init__.py
@@ -29,6 +29,6 @@ are build out of these primitives.
 
 """
 
-__version__ = "0.1+"
+__version__ = "0.2"
 
 __all__ = ["canvas", "color", "graph", "path", "tex", "unit", "graph", "trafo", "epsfile", "data"]
