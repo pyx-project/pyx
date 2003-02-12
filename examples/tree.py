954ce86759e49bcdd30b04ae62845dc96e846e7a
Andre Wobst <wobsta@pyx-project.org>
2003-02-12 10:56:48 +0000

    schmidg contributions

--- examples/tree.py
+++ examples/tree.py
@@ -1,3 +1,6 @@
+# fractal tree
+# contributed by Gerhard Schmid and Andre Wobst
+
 # extend module search path - not needed when PyX is installed properly
 import sys; sys.path[:0] = [".."]
 
@@ -7,13 +10,13 @@ from pyx import *
 l = 5
 
 # base transformations for the left, center, and right part of the tree
-ltrafo = trafo.rotate(65).scaled(0.5).translated(0, l*2.0/3.0)
-ctrafo = trafo.rotate(-4).scaled(0.7).translated(0, l)
-rtrafo = trafo.mirror(90).rotated(-65).scaled(0.5).translated(0, l)
+ltrafo = trafo.rotate(65).scaled(0.4).translated(0, l * 2.0 / 3.0)
+ctrafo = trafo.rotate(-4).scaled(0.75).translated(0, l)
+rtrafo = trafo.mirror(90).rotated(-65).scaled(0.35).translated(0, l)
 
 def tree(depth):
     "returns a list of transformations for a recursive tree of given depth"
-    r = [trafo.trafo()]
+    r = [trafo.rotate(5)]
     if depth > 0:
         subtree = tree(depth - 1)
         r.extend([t*ltrafo for t in subtree])
@@ -22,7 +25,7 @@ def tree(depth):
     return r
 
 c = canvas.canvas()
-for t in tree(5):
+for t in tree(7):
     # apply the transformation to a "sub"-canvas and insert it into the "main" canvas
     c.insert(canvas.canvas(t).stroke(path.line(0, 0, 0, l)))
 c.writetofile("tree", paperformat="a4")
