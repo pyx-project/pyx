4c5aabcd2d702ee1b54fcb45cb7d4ce87ac67ff0
Andre Wobst <wobsta@pyx-project.org>
2003-03-05 15:27:19 +0000

    scale text; no sizedef/sizesub/several texrunners needed anymore

--- examples/pattern.py
+++ examples/pattern.py
@@ -1,15 +1,9 @@
 from pyx import *
 
 p = canvas.pattern()
-t = text.texrunner()
-p.settexrunner(t)
 p.text(0, 0, r"\PyX")
 
-text.set(mode="latex")
-text.preamble(r"\usepackage{sizesub}", text.texmessage.ignore)
-text.preamble(r"\usepackage[200]{sizedef}", text.texmessage.ignore)
-
 c = canvas.canvas()
-c.text(0, 0, r"\PyX", p)
-c.writetofile("pattern", paperformat="a4")
+c.text(0, 0, r"\PyX", trafo.scale(25), p)
+c.writetofile("pattern")
 
