7222495e34e21456652bd8cad7fde1ce22fc1835
Joerg Lehmann <joerg@pyx-project.org>
2003-03-09 15:11:07 +0000

    fix test

--- test/functional/test_unit.py
+++ test/functional/test_unit.py
@@ -7,11 +7,11 @@ from pyx.path import *
 c=canvas.canvas()
 
 unit.set(uscale=1)
-c.draw(path(moveto(0,0), lineto(unit.length("1 u")+unit.length("1 t"), "1 u")), color.rgb.red)
+c.stroke(path(moveto(0,0), lineto(unit.length("1 u")+unit.length("1 t"), "1 u")), color.rgb.red)
 unit.set(uscale=2)
-c.draw(path(moveto(0,0), lineto(unit.length("1 u")+unit.length("1 t"), "1 u")), color.rgb.green)
+c.stroke(path(moveto(0,0), lineto(unit.length("1 u")+unit.length("1 t"), "1 u")), color.rgb.green)
 unit.set(uscale=4)
-c.draw(path(moveto(0,0), lineto(unit.length("1 u")+unit.length("1 t"), "1 u")), color.rgb.blue)
+c.stroke(path(moveto(0,0), lineto(unit.length("1 u")+unit.length("1 t"), "1 u")), color.rgb.blue)
 
 c.writetofile("test_unit")
 
