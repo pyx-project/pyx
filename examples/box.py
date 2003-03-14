d3c94c1c2608d243c4f47a9289e98477e3383f2d
Andre Wobst <wobsta@pyx-project.org>
2003-03-14 11:50:20 +0000

    box example

---
+++ examples/box.py
@@ -0,0 +1,19 @@
+from math import sin, cos, pi
+from pyx import *
+
+r = 1.5
+
+# create a box list of regular polygons
+boxes = [box.polygon([(-r*sin(i*2*pi/n), r*cos(i*2*pi/n))
+                      for i in range(n)])
+         for n in range(3, 8)]
+# tile them horizontally with spacing 0
+box.htile(boxes, 0)
+
+c = canvas.canvas()
+for b in boxes:
+    # plot the boxes path
+    c.stroke(b.path(), color.rgb.green)
+    # a second time with bezier rounded corners
+    c.stroke(b.path(bezierradius=0.5), color.rgb.red)
+c.writetofile("box")
