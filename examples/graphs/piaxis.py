8d3b0020d253502e33690f109fbe0dc664c2f75e
Andre Wobst <wobsta@pyx-project.org>
2003-03-18 12:28:06 +0000

    some sourcecode reformating

--- examples/graphs/piaxis.py
+++ examples/graphs/piaxis.py
@@ -1,10 +1,13 @@
-from math import sin, cos, pi
+from math import pi
 from pyx import *
 
 c = canvas.canvas()
 g = c.insert(graph.graphxy(width=8,
                            autokey=graph.key(pos="tr"),
-                           x=graph.linaxis(min=0, max=2*pi, divisor=pi, suffix=r"\pi")))
+                           x=graph.linaxis(min=0, max=2*pi, title="x",
+                                           divisor=pi, suffix=r"\pi"),
+                           y=graph.linaxis(title="y")))
+
 g.plot(graph.function("y=sin(x)"))
 g.plot(graph.function("y=cos(x)"))
 c.writetofile("piaxis")
