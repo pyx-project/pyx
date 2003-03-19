2d2d73d974840f7cdb6bf574df35790dccd660a3
Joerg Lehmann <joerg@pyx-project.org>
2003-03-19 09:59:46 +0000

    renamed logo.pyx -> pyxlogo.py

--- www/png/logo.py
+++
@@ -1,6 +0,0 @@
-from pyx import *
-
-c = canvas.canvas()
-c.text(0, 0, r"\PyX", trafo.scale(5), color.rgb.red)
-c.writetofile("logo")
-
---
+++ www/png/pyxlogo.py
@@ -0,0 +1,6 @@
+from pyx import *
+
+c = canvas.canvas()
+c.text(0, 0, r"\PyX", trafo.scale(5))
+c.writetofile("pyxlogo")
+
