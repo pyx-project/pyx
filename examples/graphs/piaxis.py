51e68441d618eb6674cf985732890ed509755afb
Andre Wobst <wobsta@pyx-project.org>
2003-03-19 17:46:59 +0000

    titles/keys layout

--- examples/graphs/piaxis.py
+++ examples/graphs/piaxis.py
@@ -3,10 +3,10 @@ from pyx import *
 
 c = canvas.canvas()
 g = c.insert(graph.graphxy(width=8, key=graph.key(pos="bl"),
-                           x=graph.linaxis(min=0, max=2*pi, title="x",
+                           x=graph.linaxis(min=0, max=2*pi, title="$x$",
                                            divisor=pi, suffix=r"\pi"),
-                           y=graph.linaxis(title="y")))
+                           y=graph.linaxis(title="$y$")))
 
-g.plot(graph.function("y=sin(x)"))
-g.plot(graph.function("y=cos(x)"))
+g.plot(graph.function("y=sin(x)", title=r"$y=\sin(x)$"))
+g.plot(graph.function("y=cos(x)", title=r"$y=\cos(x)$"))
 c.writetofile("piaxis")
