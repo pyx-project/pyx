710795ba9dc1d0c155ad12b959650a4edbf00299
Andre Wobst <wobsta@pyx-project.org>
2003-03-28 15:52:55 +0000

    basically finished all documentation; work on examples

--- examples/graphs/arrows.py
+++ examples/graphs/arrows.py
@@ -1,16 +1,24 @@
-from math import pi
+from math import pi, sqrt, atan2
 from pyx import *
 
+z1 = 2.5 + 4.5j
+z2 = 7.5 + 5.5j
+
+# we abuse a parametric function below, so we express everything in terms of a parameter k
+x = lambda k: int(k)/11
+y = lambda k: int(k)%11
+z = lambda k: x(k) + y(k) * 1j
+f = lambda k: 1/(z(k)-z1)/(z(k)-z2)                # function to be plotted
+s = lambda k: 5*sqrt(f(k).real**2 + f(k).imag**2)  # norm of the function
+a = lambda k: 180/pi*atan2(f(k).imag, f(k).real)   # direction of the function
+
 c = canvas.canvas()
-div = lambda x, y: int(x)/int(y)
-mod = lambda x, y: int(x)%int(y)
 g = c.insert(graph.graphxy(width=8,
                            x=graph.linaxis(min=0, max=10),
                            y=graph.linaxis(min=0, max=10)))
-
-g.plot(graph.paramfunction("k", 0, 120, "x, y, size, angle = mod(k, 11), div(k, 11), (1+sin(k*pi/120))/2, 3*k",
-                           points=121,
-                           context=locals()),
+g.plot(graph.paramfunction("k", 0, 120,
+                           "x, y, size, angle = x(k), y(k), s(k), a(k)",
+                           points=121, context=locals()),
        style=graph.arrow())
 
 c.writetofile("arrows")
