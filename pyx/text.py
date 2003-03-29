e40c72da7268160db843a493dce850f11a050869
Joerg Lehmann <joerg@pyx-project.org>
2003-03-29 19:22:52 +0000

    remove obsolete comment

--- pyx/text.py
+++ pyx/text.py
@@ -20,8 +20,6 @@
 # along with PyX; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
-# this code will be part of PyX 0.3
 # (TODO: check whether Knuth's division can be simplified within Python)
 #       seems to be the case!
 
