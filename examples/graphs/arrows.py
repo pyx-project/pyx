532bdd37cb9e76eac0f9e5e698decac9ba8f0c13
Joerg Lehmann <joerg@pyx-project.org>
2003-03-29 18:15:22 +0000

    simplification

--- examples/graphs/arrows.py
+++ examples/graphs/arrows.py
@@ -1,4 +1,4 @@
-from math import pi, sqrt, atan2
+from math import pi, atan2
 from pyx import *
 
 z1 = 2.5 + 4.5j
@@ -9,7 +9,7 @@ x = lambda k: int(k)/11
 y = lambda k: int(k)%11
 z = lambda k: x(k) + y(k) * 1j
 f = lambda k: 1/(z(k)-z1)/(z(k)-z2)                # function to be plotted
-s = lambda k: 5*sqrt(f(k).real**2 + f(k).imag**2)  # norm of the function
+s = lambda k: 5*abs(f(k))                          # norm of the function
 a = lambda k: 180/pi*atan2(f(k).imag, f(k).real)   # direction of the function
 
 g = graph.graphxy(width=8,
