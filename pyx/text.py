7b2a68f45d25a110380655cfe2f0d85ab09eac58
Andre Wobst <wobsta@pyx-project.org>
2003-03-31 16:12:47 +0000

    lfs handling

--- pyx/text.py
+++ pyx/text.py
@@ -1577,13 +1577,23 @@ class texrunner:
                             SysLfsName = os.path.join(os.path.dirname(__file__), "lfs", str(self.lfs) + ".lfs")
                             lfsdef = open(SysLfsName, "r").read()
                     except IOError:
-                        files = map(lambda x: x[:-4],
-                                    filter(lambda x: x[-4:] == ".lfs",
-                                           os.listdir(".") +
-                                           os.listdir(os.path.join(sys.prefix, "share", "pyx")),
-                                           os.listdir(os.path.join(os.path.dirname(__file__), "lfs"))))
-                        raise IOError("file '%s.lfs' not found. Available latex font sizes:\n%s" % (lfs, files))
+                        allfiles = []
+                        try:
+                            allfiles += os.listdir(".")
+                        except OSError:
+                            pass
+                        try:
+                            allfiles += os.listdir(os.path.join(sys.prefix, "share", "pyx"))
+                        except OSError:
+                            pass
+                        try:
+                            allfiles += os.listdir(os.path.join(os.path.dirname(__file__), "lfs"))
+                        except OSError:
+                            pass
+                        files = map(lambda x: x[:-4], filter(lambda x: x[-4:] == ".lfs", allfiles))
+                        raise IOError("file '%s.lfs' not found. Available latex font sizes:\n%s" % (self.lfs, files))
                 self.execute(lfsdef)
+                self.execute("\\normalsize%\n")
                 self.execute("\\newdimen\\linewidth%\n")
             elif self.mode == "latex":
                 if self.docopt is not None:
