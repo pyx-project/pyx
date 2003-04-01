0b29d79d81dcd66b54a78e0831643c71faa264f5
Andre Wobst <wobsta@pyx-project.org>
2003-04-01 10:56:22 +0000

    inserted access to the texrunner

--- examples/vector.py
+++ examples/vector.py
@@ -1,11 +1,12 @@
-import math
+from math import pi, sin, cos
 from pyx import *
 
-def vector(x1, y1, x2, y2, t, pos=0.5, distance=0.1):
+def vector(x1, y1, x2, y2, t, pos=0.5, distance=0.1,
+           texrunner=text.defaulttexrunner):
     c = canvas.canvas()
     c.stroke(path.line(x1, y1, x2, y2), canvas.earrow.normal)
-    textbox = text.text((1-pos)*x1 + pos*x2, (1-pos)*y1 + pos*y2, t,
-                        text.halign.center, text.vshift.mathaxis)
+    textbox = texrunner.text((1-pos)*x1 + pos*x2, (1-pos)*y1 + pos*y2, t,
+                             text.halign.center, text.vshift.mathaxis)
     if distance < 0:
         textbox.linealign(-distance, y1 - y2, x2 - x1)
     else:
@@ -17,7 +18,7 @@ r = 1.5
 a = 150
 
 c = canvas.canvas()
-dx, dy = math.cos(a * math.pi / 180), math.sin(a * math.pi / 180)
+dx, dy = cos(a * pi / 180), sin(a * pi / 180)
 x, y = r * dx, r * dy
 c.stroke(path.circle(0, 0, r))
 c.insert(vector(0, 0, x, y, r"$\vec r$"))
