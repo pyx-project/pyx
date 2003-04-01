a1b578ce9395eb4a78c14fec558c9f02b5551844
Andre Wobst <wobsta@pyx-project.org>
2003-04-01 09:00:05 +0000

    restore hello

--- examples/hello.py
+++ examples/hello.py
@@ -1,8 +1,6 @@
 from pyx import *
 
 c = canvas.canvas()
-text.set(mode="latex",docopt="nomessages")
-text.preamble(r"\usepackage{type1cm}\usepackage[12]{sizedef}\newcommand{\un}[1]{\ensuremath{\unskip\,\mathrm{#1}}}")
-c.text(0, 0, r"Hello, world!")
+c.text(0, 0, "Hello, world!")
 c.stroke(path.line(0, 0, 2, 0))
 c.writetofile("hello")
