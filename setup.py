4ead4a14207c9919cc77220b9521a3be44099d22
Andre Wobst <wobsta@pyx-project.org>
2003-04-02 11:13:15 +0000

    merging trees

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+
+"""Python package for the generation of encapsulated PostScript figures
+
+PyX is a Python package for the creation of encapsulated PostScript figures.
+It provides both an abstraction of PostScript and a TeX/LaTeX interface.
+Complex tasks like 2d and 3d plots in publication-ready quality are built out
+of these primitives.
+"""
 
 from distutils.core import setup, Extension
+import sys
 import pyx
 
 ext_modules = [Extension("pyx/t1strip/_t1strip",
@@ -20,13 +30,35 @@ data_files = [('share/pyx', ['pyx/lfs/10pt.lfs',
                              'pyx/lfs/foils25pt.lfs',
                              'pyx/lfs/foils30pt.lfs'])]
 
+if sys.version_info >= (2, 3):
+    addargs = { "classifiers":
+                ["Development Status :: 3 - Alpha",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: End Users/Desktop",
+                 "License :: OSI Approved :: GNU General Public License (GPL)",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python",
+                 "Topic :: Multimedia :: Graphics",
+                 "Topic :: Scientific/Engineering :: Visualization",
+		 "Topic :: Software Development :: Libraries :: Python Modules"]
+              }
+else:
+    addargs = {}
+
+# We're using the module docstring as the distutils descriptions. (seen in Zope3 setup.py)
+doclines = __doc__.split("\n")
+
 setup(name="PyX",
       version=pyx.__version__,
       author="Jörg Lehmann, André Wobst",
       author_email="pyx-devel@lists.sourceforge.net",
       url="http://pyx.sourceforge.net/",
-      description="Python package for the generation of mixed PS and (La)TeX code",
+      download_url="http://sourceforge.net/project/showfiles.php?group_id=45430",
+      description=doclines[0],
+      long_description="\n".join(doclines[2:]),
       license="GPL",
+      platform="OS independent",
       packages=['pyx', 'pyx/t1strip', 'pyx/pykpathsea'],
       ext_modules=ext_modules,
-      data_files=data_files)
+      data_files=data_files,
+      **addargs)
