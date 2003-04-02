cc5301214730779479c987bd717d99e0dedc335d
Andre Wobst <wobsta@pyx-project.org>
2003-04-02 10:23:06 +0000

    from 0.3.0+ tree: renamed vbox to parbox (was done in 0.3 partially only)

--- examples/valign.py
+++ examples/valign.py
@@ -4,18 +4,19 @@ c = canvas.canvas()
 
 # apply global TeX setting
 text.preamble(r"\parindent=0pt")
+w = 1.2 # an appropriate parbox width
 
 # vertical alignments by margins
 c.stroke(path.line(0, 4, 6, 4), canvas.linewidth.THin)
-c.text(0, 4, r"spam \& eggs", text.vbox(1.2), text.valign.top)
-c.text(2, 4, r"spam \& eggs", text.vbox(1.2), text.valign.middle)
-c.text(4, 4, r"spam \& eggs", text.vbox(1.2), text.valign.bottom)
+c.text(0, 4, r"spam \& eggs", text.parbox(w), text.valign.top)
+c.text(2, 4, r"spam \& eggs", text.parbox(w), text.valign.middle)
+c.text(4, 4, r"spam \& eggs", text.parbox(w), text.valign.bottom)
 
 # vertical alignments by baselines
 c.stroke(path.line(0, 2, 6, 2), canvas.linewidth.THin)
-c.text(0, 2, r"spam \& eggs", text.vbox(1.2), text.valign.topbaseline)
-c.text(2, 2, r"spam \& eggs", text.vbox(1.2), text.valign.middlebaseline)
-c.text(4, 2, r"spam \& eggs", text.vbox(1.2), text.valign.bottombaseline)
+c.text(0, 2, r"spam \& eggs", text.parbox(w), text.valign.topbaseline)
+c.text(2, 2, r"spam \& eggs", text.parbox(w), text.valign.middlebaseline)
+c.text(4, 2, r"spam \& eggs", text.parbox(w), text.valign.bottombaseline)
 
 # vertical shifts
 c.stroke(path.line(0, 0, 8, 0), canvas.linewidth.THin)
