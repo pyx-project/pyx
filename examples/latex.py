dcd57a82afdf090077e92250fd96396c0859a7f3
Andre Wobst <wobsta@pyx-project.org>
2003-04-03 07:32:04 +0000

    doc

--- examples/latex.py
+++ examples/latex.py
@@ -5,11 +5,11 @@ text.set(mode="latex")
 
 c = canvas.canvas()
 # the canvas, by default, uses the defaulttexrunner from the text module
-# (this can be changed by the canvas settexrunner method)
+# (this can be changed by the canvas method settexrunner)
 c.text(0, 0, r"This is \LaTeX.")
 
 # you can have several texrunners (several running TeX/LaTeX instances)
-plaintex = text.texrunner()
+plaintex = text.texrunner() # plain TeX instance
 c.insert(plaintex.text(0, -1, r"This is plain \TeX."))
 
 c.writetofile("latex")
