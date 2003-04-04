70d2551d9feb188ebea5e7ec666f65ee2a8b54d5
Joerg Lehmann <joerg@pyx-project.org>
2003-04-04 13:44:59 +0000

    somewhat better

--- examples/graphs/integral.py
+++ examples/graphs/integral.py
@@ -1,16 +1,16 @@
 from pyx import *
 
 a, b = 2, 9 # integral area
-unit.set(wscale = 2) # increase line widths, but keep all other scales
 
-apainter = graph.axispainter(baselineattrs=canvas.earrow.normal)
+p = graph.axispainter(baselineattrs=canvas.earrow.normal,
+                      titlepos=0.98, titledirection=None)
 xpart = graph.manualpart(ticks=(graph.frac(a, 1), graph.frac(b, 1)),
-                         texts=("a", "b")) # ticks at a and b
-c = canvas.canvas()
-g = c.insert(graph.graphxy(width=8, x2=None, y2=None,
-                           x=graph.linaxis(min=0, max=10, part=xpart,
-                                           painter=apainter),
-                           y=graph.linaxis(painter=apainter, part=None)))
+                         texts=("$a$", "$b$")) # ticks at a and b
+g = graph.graphxy(width=8, x2=None, y2=None,
+                  x=graph.linaxis(title="$x$", min=0, max=10,
+                                  part=xpart, painter=p),
+                  y=graph.linaxis(title="$y$",
+                                  part=None, painter=p))
 style = g.plot(graph.function("y=(x-3)*(x-5)*(x-7)")).style
 g.finish()
 
@@ -22,9 +22,8 @@ area = (pa.split(splitpa)[0] <<
         style.path.split(splita, splitb)[1] <<
         pb.split(splitpb)[0].reversed())
 area.append(path.closepath())
-g.stroke(area, canvas.linewidth.Thick, canvas.filled(color.gray(0.8)))
-c.text(g.pos(0.5 * (a + b), 0)[0], 1,
+g.stroke(area, canvas.filled(color.gray(0.8)))
+g.text(g.pos(0.5 * (a + b), 0)[0], 1,
        r"\int_a^b f(x){\rm d}x", text.halign.center, text.mathmode)
-
-c.writetofile("integral")
+g.writetofile("integral")
 
