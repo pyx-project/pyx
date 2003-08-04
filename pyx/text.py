62c56017b17a1cbc30529fce3944b4460464f29e
Joerg Lehmann <joerg@pyx-project.org>
2003-08-04 18:06:48 +0000

    - more steps towards font encoding support

--- pyx/text.py
+++ pyx/text.py
@@ -102,6 +102,31 @@ class binfile:
         assert l <= bytes-1, "inconsistency in file: string too long"
         return self.file.read(bytes-1)[:l]
 
+class tokenfile:
+    """ ascii file containing tokens separated by spaces.
+
+    Comments beginning with % are ignored. Strings containing spaces
+    are not handled correctly
+    """
+
+    def __init__(self, filename):
+        self.file = open(filename, "r")
+        self.line = None
+
+    def gettoken(self):
+        """ return next token or None if EOF """
+        while not self.line:
+            line = self.file.readline()
+            if line == "":
+                return None
+            self.line = line.split("%")[0].split()
+        token = self.line[0]
+        self.line = self.line[1:]
+        return token
+
+    def close(self):
+        self.file.close()
+
 
 class DVIError(exceptions.Exception): pass
 
@@ -304,13 +329,48 @@ class TFMFile:
         self.file.file.close()
 
 
+class FontEncoding:
+
+    def __init__(self, filename):
+        """ font encoding contained in filename """
+        encpath = pykpathsea.find_file(filename, pykpathsea.kpse_tex_ps_header_format)
+        encfile = tokenfile(encpath)
+
+        # name of encoding
+        self.encname = encfile.gettoken()
+        token = encfile.gettoken()
+        if token != "[":
+            raise RuntimeError("cannot parse encoding file '%s', expecting '[' got '%s'" % (filename, token))
+        self.encvector = []
+        for i in range(256):
+            token = encfile.gettoken()
+            if token is None or token=="]":
+                raise RuntimeError("not enough charcodes in encoding file '%s'" % filename)
+            self.encvector.append(token)
+        if encfile.gettoken() != "]":
+            raise RuntimeError("too many charcodes in encoding file '%s'" % filename)
+        token = encfile.gettoken()
+        if token != "def":
+            raise RuntimeError("cannot parse encoding file '%s', expecting 'def' got '%s'" % (filename, token))
+        token = encfile.gettoken()
+        if token != None:
+            raise RuntimeError("encoding file '%s' too long" % filename)
+        encfile.close()
+
+    def encode(self, charcode):
+        return self.encvector[charcode]
+
+
 class FontMapping:
 
-    tokenpattern = re.compile(r'"(.*?)("\s+|$)|(.*?)(\s+|$)')
+    tokenpattern = re.compile(r'"(.*?)("\s+|"$|$)|(.*?)(\s+|$)')
 
     def __init__(self, s):
         """ construct font mapping from line s of dvips mapping file """
-        self.texname = self.psname = self.fontfile = self.encoding = self.pscode = None
+        self.texname = self.psname = self.fontfile = self.encoding = None
+
+        # supported postscript fragments occuring in psfonts.map
+        self.reencodefont = self.extendfont = self.slantfont = None
 
         tokens = []
         while len(s):
@@ -338,7 +398,22 @@ class FontMapping:
                 else:
                     raise RuntimeError("wrong syntax in font catalog file 'psfonts.map'")
             elif token.startswith('"'):
-                self.pscode = token[1:-1]
+                pscode = token[1:-1].split()
+                # parse standard postscript code fragments
+                while pscode:
+                    try:
+                        arg, cmd = pscode[:2]
+                    except:
+                        raise RuntimeError("Unsupported Postscript fragment '%s' in psfonts.map" % pscode) 
+                    pscode = pscode[2:]
+                    if cmd == "ReEncodeFont":
+                        self.reencodefont = arg
+                    elif cmd == "ExtendFont":
+                        self.extendfont = arg
+                    elif cmd == "SlantFont":
+                        self.slantfont = arg
+                    else:
+                        raise RuntimeError("Unsupported Postscript fragment '%s %s' in psfonts.map" % (arg, cmd)) 
             else:
                 if self.texname is None:
                     self.texname = token
@@ -348,8 +423,8 @@ class FontMapping:
             self.psname = self.texname
 
     def __str__(self):
-        return ("'%s' is '%s' read from '%s' encoded as '%s' used with '%s'" %
-                (self.texname, self.psname, self.fontfile, repr(self.encoding), repr(self.pscode)))
+        return ("'%s' is '%s' read from '%s' encoded as '%s'" %
+                (self.texname, self.psname, self.fontfile, repr(self.encoding)))
 
 
 # generate fontmap
