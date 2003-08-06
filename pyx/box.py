9f6cc21e3e953f59d27f93745f0592743706e3c8
Joerg Lehmann <joerg@pyx-project.org>
2003-08-06 08:46:58 +0000

    prune unused imports

--- pyx/box.py
+++ pyx/box.py
@@ -21,7 +21,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-import types, re, math, string, sys
+import types, math
 import bbox, path, unit, trafo, helper
 
 
