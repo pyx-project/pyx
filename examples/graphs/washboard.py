f21e3adfde832a439ae8d02f464fc05c1dbcc570
Joerg Lehmann <joerg@pyx-project.org>
2003-11-27 14:05:33 +0000

    more examples work again

--- examples/graphs/washboard.py
+++ examples/graphs/washboard.py
@@ -6,7 +6,7 @@ from pyx.deco import barrow, earrow
 from pyx.style import linewidth, linestyle
 from pyx.graph import graphxy, linaxis, axispainter, function, line
 
-mypainter = axispainter(baselineattrs=earrow.normal, titlepos=1)
+mypainter = axispainter(baselineattrs=earrow.normal(), titlepos=1)
 def mycos(x): return -cos(x)+.10*x
 
 g = graphxy(height=5, x2=None, y2=None,
@@ -24,8 +24,8 @@ x3, y3 = g.pos(pi+.1, mycos(pi+.1))
 g.stroke(path.line(x1-.5, y1, x1+.5, y1), linestyle.dashed)
 g.stroke(path.line(x1-.5, y3, x3+.5, y3), linestyle.dashed)
 g.stroke(path.line(x2-.5, y2, x3+.5, y2), linestyle.dashed)
-g.stroke(path.line(x1, y1, x1, y3), barrow.normal, earrow.normal)
-g.stroke(path.line(x3, y2, x3, y3), barrow.normal, earrow.normal)
+g.stroke(path.line(x1, y1, x1, y3), barrow.normal(), earrow.normal())
+g.stroke(path.line(x3, y2, x3, y3), barrow.normal(), earrow.normal())
 g.text(x1+.2, 0.5*(y1+y3), r"$2\pi\gamma k\Omega$", text.vshift.middlezero)
 g.text(x1-.6, y1-.1, r"$E_{\rm b}$", text.halign.right)
 g.text(x3+.15, y2+.20, r"$2J_k(\varepsilon/\Omega)+\pi\gamma k\Omega$")
--- examples/vector.py
+++ examples/vector.py
@@ -4,7 +4,7 @@ from pyx import *
 def vector(x1, y1, x2, y2, t, pos=0.5, distance=0.1,
            texrunner=text.defaulttexrunner):
     c = canvas.canvas()
-    c.stroke(path.line(x1, y1, x2, y2), canvas.earrow.normal)
+    c.stroke(path.line(x1, y1, x2, y2), deco.earrow.normal())
     textbox = texrunner.text((1-pos)*x1 + pos*x2, (1-pos)*y1 + pos*y2, t,
                              text.halign.center, text.vshift.mathaxis)
     if distance < 0:
