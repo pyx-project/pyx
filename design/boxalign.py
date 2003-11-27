6e30542c0e3cf3dcaa87d547e8f87e3f50902bad
Andre Wobst <wobsta@pyx-project.org>
2003-11-27 15:47:22 +0000

    make boxalign working with current CVS

--- design/boxalign.py
+++ design/boxalign.py
@@ -3,56 +3,73 @@ import sys, math
 sys.path[:0] = [".."]
 from pyx import *
 
-c = canvas.canvas()
-t = c.insert(tex.tex())
-c.stroke(path.line(0, 0, 4, 2), canvas.earrow.Large)
-graph.textbox(t, r"$\vec p$").linealign(0.1, -2/math.sqrt(20), 4/math.sqrt(20)).printtext(2, 1)
-c.stroke(path.line(0, 0, 1, 1), canvas.earrow.Large)
-graph.textbox(t, r"$\vec a$").linealign(0.1, -1/math.sqrt(2), 1/math.sqrt(2)).printtext(0.5, 0.5)
-c.stroke(path.line(-1, 3, 3, -1), canvas.linestyle.dotted)
-c.stroke(path.line(1, 1, 1-1/math.sqrt(2), 1+1/math.sqrt(2)), canvas.earrow.Large)
-graph.textbox(t, r"$\vec b$").linealign(0.1, 1/math.sqrt(2), 1/math.sqrt(2)).printtext(1-1/math.sqrt(8), 1+1/math.sqrt(8))
-c.writetofile("boxalignpal")
-
-###############################################################################
-
-c = canvas.canvas()
-t = c.insert(tex.tex())
-c.stroke(path.line(0, 0, 4, 2), canvas.earrow.Large)
-graph.textbox(t, r"$\vec p$").linealign(0.1, -2/math.sqrt(20), 4/math.sqrt(20)).printtext(2, 1)
-c.stroke(path.line(0, 0, -1, 1), canvas.earrow.Large)
-graph.textbox(t, r"$\vec a$").linealign(0.1, -1/math.sqrt(2), -1/math.sqrt(2)).printtext(-0.5, 0.5)
-c.stroke(path.path(path.arc(0, 0, math.sqrt(2), 0, 360), path.closepath()), canvas.linestyle.dotted)
-c.writetofile("boxalignpac")
-
-###############################################################################
-
-c = canvas.canvas()
-t = c.insert(tex.tex())
-c.stroke(path.line(0, 0, 4, 2), canvas.earrow.Large)
-graph.textbox(t, r"$\vec e$").linealign(0.1, -2/math.sqrt(20), 4/math.sqrt(20)).printtext(2, 1)
-c.stroke(path.line(0, 0, 6, 2), canvas.earrow.Large)
-graph.textbox(t, r"$\vec f$").linealign(0.1, 2/math.sqrt(60), -6/math.sqrt(60)).printtext(3, 1)
-c.stroke(path.line(6, 2, 4, 2), canvas.earrow.Large)
-graph.textbox(t, r"$\vec g$").linealign(0.1, 0, 1).printtext(5, 2)
-c.stroke(path.line(0, 0, -1, 1), canvas.earrow.Large)
-graph.textbox(t, r"$\vec a$").linealign(0.1, -1/math.sqrt(2), -1/math.sqrt(2)).printtext(-0.5, 0.5)
-c.stroke(path.path(path.arc(0, 0, math.sqrt(2), 0, 360), path.closepath()), canvas.linestyle.dotted)
-c.stroke(path.line(0, 0, 0, math.sqrt(2)), canvas.earrow.Large)
-graph.textbox(t, r"$\vec b$").linealign(0.1, -1, 0).printtext(0, 1/math.sqrt(2))
-c.writetofile("boxalignlac")
-
-###############################################################################
+#c = canvas.canvas()
+#c.stroke(path.line(0, 0, 4, 2), deco.earrow.Large())
+#t = text.text(2, 1, r"$\vec p$")
+#t.linealign(0.1, -2/math.sqrt(20), 4/math.sqrt(20))
+#c.insert(t)
+#c.stroke(path.line(0, 0, 1, 1), deco.earrow.Large())
+#t = text.text(0.5, 0.5, r"$\vec a$")
+#t.linealign(0.1, -1/math.sqrt(2), 1/math.sqrt(2))
+#c.insert(t)
+#c.stroke(path.line(-1, 3, 3, -1), style.linestyle.dotted)
+#c.stroke(path.line(1, 1, 1-1/math.sqrt(2), 1+1/math.sqrt(2)), deco.earrow.Large())
+#t = text.text(1-1/math.sqrt(8), 1+1/math.sqrt(8), r"$\vec b$")
+#t.linealign(0.1, 1/math.sqrt(2), 1/math.sqrt(2))
+#c.insert(t)
+#c.writetofile("boxalignpal")
+#
+################################################################################
+#
+#c = canvas.canvas()
+#c.stroke(path.line(0, 0, 4, 2), deco.earrow.Large())
+#t = text.text(2, 1, r"$\vec p$")
+#t.linealign(0.1, -2/math.sqrt(20), 4/math.sqrt(20))
+#c.insert(t)
+#c.stroke(path.line(0, 0, -1, 1), deco.earrow.Large())
+#t = text.text(-0.5, 0.5, r"$\vec a$")
+#t.linealign(0.1, -1/math.sqrt(2), -1/math.sqrt(2))
+#c.insert(t)
+#c.stroke(path.path(path.arc(0, 0, math.sqrt(2), 0, 360), path.closepath()), style.linestyle.dotted)
+#c.writetofile("boxalignpac")
+#
+################################################################################
+#
+#c = canvas.canvas()
+#c.stroke(path.line(0, 0, 4, 2), deco.earrow.Large())
+#t = text.text(2, 1, r"$\vec e$")
+#t.linealign(0.1, -2/math.sqrt(20), 4/math.sqrt(20))
+#c.insert(t)
+#c.stroke(path.line(0, 0, 6, 2), deco.earrow.Large())
+#t = text.text(3, 1, r"$\vec f$")
+#t.linealign(0.1, 2/math.sqrt(60), -6/math.sqrt(60))
+#c.insert(t)
+#c.stroke(path.line(6, 2, 4, 2), deco.earrow.Large())
+#t = text.text(5, 2, r"$\vec g$")
+#t.linealign(0.1, 0, 1)
+#c.insert(t)
+#c.stroke(path.line(0, 0, -1, 1), deco.earrow.Large())
+#t = text.text(-0.5, 0.5, r"$\vec a$")
+#t.linealign(0.1, -1/math.sqrt(2), -1/math.sqrt(2))
+#c.insert(t)
+#c.stroke(path.path(path.arc(0, 0, math.sqrt(2), 0, 360), path.closepath()), style.linestyle.dotted)
+#c.stroke(path.line(0, 0, 0, math.sqrt(2)), deco.earrow.Large())
+#t = text.text(0, 1/math.sqrt(2), r"$\vec b$")
+#t.linealign(0.1, -1, 0)
+#c.insert(t)
+#c.writetofile("boxalignlac")
+#
+################################################################################
 
 def drawexample(canvas, corner, linealign):
-    if corner:
-        b = graph.alignbox((0, 0), (0, 0), (1, 0), (0.5, math.sqrt(3)/2))
-    else:
-        b = graph.alignbox((0.5, math.sqrt(3)/6), (0, 0), (1, 0), (0.5, math.sqrt(3)/2))
     r = 1.5
     canvas.stroke(path.path(path.arc(0, 0, r, 0, 360)))
     phi = 0
     while phi < 2 * math.pi + 1e-10:
+        if corner:
+            b = box.polygon(center=(0, 0), corners=[(0, 0), (1, 0), (0.5, math.sqrt(3)/2)])
+        else:
+            b = box.polygon(center=(0, 0), corners=[(-0.5, -math.sqrt(3)/6), (0.5, -math.sqrt(3)/6), (0, math.sqrt(3)/3)])
         if linealign:
             b.linealign(r, math.cos(phi), math.sin(phi))
         else:
@@ -65,19 +82,18 @@ def drawexample(canvas, corner, linealign):
 
 d = 6
 c = canvas.canvas()
-sc = c.insert(canvas.canvas(trafo.translation(0, d)))
+sc = c.insert(canvas.canvas(trafo.translate(0, d)))
 drawexample(sc, 0, 0)
-sc = c.insert(canvas.canvas(trafo.translation(d, d)))
+sc = c.insert(canvas.canvas(trafo.translate(d, d)))
 drawexample(sc, 0, 1)
-sc = c.insert(canvas.canvas(trafo.translation(0, 0)))
+sc = c.insert(canvas.canvas(trafo.translate(0, 0)))
 drawexample(sc, 1, 0)
-sc = c.insert(canvas.canvas(trafo.translation(d, 0)))
+sc = c.insert(canvas.canvas(trafo.translate(d, 0)))
 drawexample(sc, 1, 1)
-t = c.insert(tex.tex())
-t.text(0, 1.5*d, "align at a circle", tex.halign.center)
-t.text(d, 1.5*d, "align at tangents", tex.halign.center)
-t.text(-0.5*d, d, "reference point at the triangle center", tex.halign.center, tex.direction.vertical)
-t.text(-0.5*d, 0, "reference point at a triangle corner", tex.halign.center, tex.direction.vertical)
+c.text(0, 1.5*d, "align at a circle", text.halign.center)
+c.text(d, 1.5*d, "align at tangents", text.halign.center)
+c.text(-0.5*d, d, "reference point at the triangle center", text.halign.center, trafo.rotate(90))
+c.text(-0.5*d, 0, "reference point at a triangle corner", text.halign.center, trafo.rotate(90))
 c.writetofile("boxalignexample")
 
 
