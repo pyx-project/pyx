e57fedb76971daceadda762506efc0c7afcee4fc
Michael Schindler <m-schindler@users.sourceforge.net>
2004-01-19 18:01:37 +0000

    added list for graph.line attributes

--- examples/graphs/change.py
+++ examples/graphs/change.py
@@ -5,6 +5,6 @@ g = graph.graphxy(width=10,
                   y=graph.linaxis(min=0, max=2))
 g.plot([graph.function("x=y**(2**(3-%i))" % i) for i in range(3)] +
        [graph.function("y=x**(2**%i)" % i) for i in range(4)],
-       graph.line(graph.changecolor.Rainbow()))
+       graph.line([graph.changecolor.Rainbow()]))
 g.writetofile("change")
 
