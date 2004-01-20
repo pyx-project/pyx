88b539b6ea203fc283b86e5d5d1b747c26a44476
Andre Wobst <wobsta@pyx-project.org>
2004-01-20 11:28:20 +0000

    avoid overlapp

--- examples/graphs/piaxis.py
+++ examples/graphs/piaxis.py
@@ -6,7 +6,7 @@ g = graph.graphxy(width=8, key=graph.key(pos="bl"),
                          texter=graph.rationaltexter(suffix=r"\pi")),
         y=graph.linaxis(title="$y$"))
 
-g.plot(graph.function("y=sin(x)", title=r"$y=\sin(x)$"))
-g.plot(graph.function("y=cos(x)", title=r"$y=\cos(x)$"))
+g.plot(graph.function("y=sin(x)", title=r"$\sin(x)$"))
+g.plot(graph.function("y=cos(x)", title=r"$\cos(x)$"))
 
 g.writetofile("piaxis")
