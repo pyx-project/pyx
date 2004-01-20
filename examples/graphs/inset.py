1de1b99621bb48bb5210dcebe9ef78862e599d1e
Andre Wobst <wobsta@pyx-project.org>
2004-01-20 07:37:38 +0000

    two small new graph examples

---
+++ examples/graphs/inset.py
@@ -0,0 +1,9 @@
+from pyx import *
+
+g = graph.graphxy(width=8, x=graph.linaxis(min=-2, max=2))
+g.plot(graph.function("y=exp(x)"))
+g2 = g.insert(graph.graphxy(width=2.5, xpos=1, ypos=3,
+                            x=graph.linaxis(min=0, max=3),
+                            y=graph.linaxis(min=-2, max=2)))
+g2.plot(graph.function("y=log(x)"))
+g.writetofile("inset")
---
+++ examples/graphs/link.py
@@ -0,0 +1,14 @@
+import math
+from pyx import *
+
+c = canvas.canvas()
+
+g1 = c.insert(graph.graphxy(width=8,
+                            x=graph.linaxis(min=0, max=1)))
+g1.plot(graph.function("y=2*exp(-30*x)-exp(-3*x)"))
+
+g2 = c.insert(graph.graphxy(width=8, ypos=g1.height+0.5,
+                            x=g1.axes["x"].createlinkaxis()))
+g2.plot(graph.function("y=cos(20*x)*exp(-2*x)"))
+
+c.writetofile("link")
