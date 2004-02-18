68d7bbe4c1eb4442ee006f355b7f253e0dd1f5b1
Michael Schindler <m-schindler@users.sourceforge.net>
2004-02-18 17:22:50 +0000

    changed "_name" to "name_pt"

--- pyx/connector.py
+++ pyx/connector.py
@@ -43,15 +43,18 @@ def _topt(length, default_type=None):
     else:
         return unit.topt(unit.length(length))
 
-class _connector(path.normpath):
+class connector_pt(path.normpath):
 
     def omitends(self, box1, box2):
         """intersect a path with the boxes' paths"""
 
+        # cut off the start of self
+        # XXX how can decoration of this box1.path() be handled?
         sp = self.intersect(box1.path())[0]
         try: self.path = self.split(sp[:1])[1].path
         except: pass
 
+        # cut off the end of self
         sp = self.intersect(box2.path())[0]
         try: self.path = self.split(sp[-1:])[0].path
         except: pass
@@ -59,13 +62,16 @@ class _connector(path.normpath):
     def shortenpath(self, dists):
         """shorten a path by the given distances"""
 
+        # cut off the start of self
+        # XXX should path.lentopar used here?
         center = [unit.topt(self.begin()[i]) for i in [0,1]]
-        sp = self.intersect( path.circle_pt(center[0], center[1], dists[0]) )[0]
+        sp = self.intersect(path.circle_pt(center[0], center[1], dists[0]))[0]
         try: self.path = self.split(sp[:1])[1].path
         except: pass
 
+        # cut off the end of self
         center = [unit.topt(self.end()[i]) for i in [0,1]]
-        sp = self.intersect( path.circle_pt(center[0], center[1], dists[1]) )[0]
+        sp = self.intersect(path.circle_pt(center[0], center[1], dists[1]))[0]
         try: self.path = self.split(sp[-1:])[0].path
         except: pass
 
@@ -75,14 +81,14 @@ class _connector(path.normpath):
 ################
 
 
-class _line(_connector):
+class line_pt(connector_pt):
 
     def __init__(self, box1, box2, boxdists=[0,0]):
 
         self.box1 = box1
         self.box2 = box2
 
-        _connector.__init__(self,
+        connector_pt.__init__(self,
             path.moveto_pt(*self.box1.center),
             path.lineto_pt(*self.box2.center))
 
@@ -90,10 +96,18 @@ class _line(_connector):
         self.shortenpath(boxdists)
 
 
-class _arc(_connector):
+class arc_pt(connector_pt):
 
     def __init__(self, box1, box2, relangle=45,
                  absbulge=None, relbulge=None, boxdists=[0,0]):
+
+        # the deviation of arc from the straight line can be specified:
+        # 1. By an angle between a straight line and the arc
+        #    This angle is measured at the centers of the box.
+        # 2. By the largest normal distance between line and arc: absbulge
+        #    or, equivalently, by the bulge relative to the length of the
+        #    straight line from center to center.
+        # Only one can be used.
 
         self.box1 = box1
         self.box2 = box2
@@ -124,8 +138,8 @@ class _arc(_connector):
             try: center = tan(0.5*math.pi - radians(relangle))
             except: center = 0
 
-        # up to here center is only
-        # the distance from the middle of the straight connection
+        # up to here center is only the distance from the middle of the
+        # straight connection
         center = [0.5 * (self.box1.center[0] + self.box2.center[0] - rel[1]*center),
                   0.5 * (self.box1.center[1] + self.box2.center[1] + rel[0]*center)]
         angle1 = atan2(*[self.box1.center[i] - center[i]  for i in [1,0]])
@@ -134,11 +148,11 @@ class _arc(_connector):
         # draw the arc in positive direction by default
         # negative direction if relangle<0 or bulge<0
         if (relangle is not None and relangle < 0) or (bulge is not None and bulge < 0):
-            _connector.__init__(self,
+            connector_pt.__init__(self,
                 path.moveto_pt(*self.box1.center),
                 path.arcn_pt(center[0], center[1], radius, degrees(angle1), degrees(angle2)))
         else:
-            _connector.__init__(self,
+            connector_pt.__init__(self,
                 path.moveto_pt(*self.box1.center),
                 path.arc_pt(center[0], center[1], radius, degrees(angle1), degrees(angle2)))
 
@@ -146,13 +160,23 @@ class _arc(_connector):
         self.shortenpath(boxdists)
 
 
-class _curve(_connector):
+class curve_pt(connector_pt):
 
     def __init__(self, box1, box2,
                  relangle1=45, relangle2=45,
                  absangle1=None, absangle2=None,
                  absbulge=0, relbulge=0.39, boxdists=[0,0]):
-        # relbulge=0.39 and relangle1,2=45 leads
+
+        # The deviation of the curve from a straight line can be specified:
+        # A. By an angle at each center
+        #    These angles are either absolute angles with origin at the positive x-axis
+        #    or the relative angle with origin at the straight connection line
+        # B. By the (expected) largest normal distance between line and arc: absbulge
+        #    and/or by the (expected) bulge relative to the length of the
+        #    straight line from center to center.
+        # Here, we need both informations.
+        #
+        # a curve with relbulge=0.39 and relangle1,2=45 leads
         # approximately to the arc with angle=45
 
         self.box1 = box1
@@ -161,9 +185,11 @@ class _curve(_connector):
         rel = [self.box2.center[0] - self.box1.center[0],
                self.box2.center[1] - self.box1.center[1]]
         distance = hypot(*rel)
+        # absolute angle of the straight connection
         dangle = atan2(rel[1], rel[0])
 
-        # calculate the armlength and angles for the control points
+        # calculate the armlength and absolute angles for the control points:
+        # absolute and relative bulges are added
         bulge = abs(distance*relbulge + absbulge)
 
         if absangle1 is not None:
@@ -175,12 +201,13 @@ class _curve(_connector):
         else:
             angle2 = dangle + radians(relangle2)
 
+        # get the control points
         control1 = [cos(angle1), sin(angle1)]
         control2 = [cos(angle2), sin(angle2)]
         control1 = [self.box1.center[i] + control1[i] * bulge  for i in [0,1]]
         control2 = [self.box2.center[i] - control2[i] * bulge  for i in [0,1]]
 
-        _connector.__init__(self,
+        connector_pt.__init__(self,
             path.moveto_pt(*self.box1.center),
             path.curveto_pt(*(control1 + control2 + helper.ensurelist(self.box2.center))))
 
@@ -188,7 +215,7 @@ class _curve(_connector):
         self.shortenpath(boxdists)
 
 
-class _twolines(_connector):
+class twolines_pt(connector_pt):
 
     def __init__(self, box1, box2,
                  absangle1=None, absangle2=None,
@@ -197,6 +224,17 @@ class _twolines(_connector):
                  bezierradius=None, beziersoftness=1,
                  arcradius=None,
                  boxdists=[0,0]):
+
+        # The connection with two lines can be done in the following ways:
+        # 1. an angle at each box-center
+        # 2. two armlengths (if they are long enough)
+        # 3. angle and armlength at the same box
+        # 4. angle and armlength at different boxes
+        # 5. one armlength and the angle between the arms
+        #
+        # Angles at the box-centers can be relative or absolute
+        # The angle in the middle is always relative
+        # lengths are always absolute
 
         self.box1 = box1
         self.box2 = box2
@@ -208,6 +246,7 @@ class _twolines(_connector):
         distance = hypot(*rel)
         dangle = atan2(rel[1], rel[0])
 
+        # find out what arguments are given:
         if relangle1 is not None: relangle1 = radians(relangle1)
         if relangle2 is not None: relangle2 = radians(relangle2)
         if relangleM is not None: relangleM = radians(relangleM)
@@ -225,17 +264,20 @@ class _twolines(_connector):
         if no_angles + no_lengths != 2:
             raise NotImplementedError, "Please specify exactly two angles or lengths"
 
-        # calculate necessary angles and sidelengths
-        # always length1 and relangle1 !
+        # calculate necessary angles and armlengths
+        # always length1 and relangle1
+
         # the case with two given angles
-        if no_angles==2:
+        # use the "sine-theorem" for calculating length1
+        if no_angles == 2:
             if relangle1 is None: relangle1 = math.pi - relangle2 - relangleM
             elif relangle2 is None: relangle2 = math.pi - relangle1 - relangleM
             elif relangleM is None: relangleM = math.pi - relangle1 - relangle2
             length1 = distance * abs(sin(relangle2)/sin(relangleM))
             middle = self._middle_a(begin, dangle, length1, relangle1)
         # the case with two given lengths
-        elif no_lengths==2:
+        # uses the "cosine-theorem" for calculating length1
+        elif no_lengths == 2:
             relangle1 = acos((distance**2 + length1**2 - length2**2) / (2.0*distance*length1))
             middle = self._middle_a(begin, dangle, length1, relangle1)
         # the case with one length and one angle
@@ -264,7 +306,7 @@ class _twolines(_connector):
             else:
                 raise NotImplementedError, "I found a strange combination of arguments"
 
-        _connector.__init__(self,
+        connector_pt.__init__(self,
             path.moveto_pt(*self.box1.center),
             path.lineto_pt(*middle),
             path.lineto_pt(*self.box2.center))
@@ -283,14 +325,14 @@ class _twolines(_connector):
 
     def _missinglength(self, lenA, lenB, angleA):
         # calculate lenC, where side A and angleA are opposite
-        tmp1 = lenB*cos(angleA)
+        tmp1 = lenB * cos(angleA)
         tmp2 = sqrt(tmp1**2 - lenB**2 + lenA**2)
-        if tmp1>tmp2: return tmp1-tmp2
-        return tmp1+tmp2
+        if tmp1 > tmp2: return tmp1 - tmp2
+        return tmp1 + tmp2
 
 
 
-class line(_line):
+class line(line_pt):
 
     """a line is the straight connector between the centers of two boxes"""
 
@@ -298,10 +340,10 @@ class line(_line):
 
         boxdists_pt = [_topt(helper.getitemno(boxdists,i), default_type="v") for i in [0,1]]
 
-        _line.__init__(self, box1, box2, boxdists=boxdists_pt)
+        line_pt.__init__(self, box1, box2, boxdists=boxdists_pt)
 
 
-class curve(_curve):
+class curve(curve_pt):
 
     """a curve is the curved connector between the centers of two boxes.
     The constructor needs both angle and bulge"""
@@ -315,13 +357,13 @@ class curve(_curve):
 
         boxdists_pt = [_topt(helper.getitemno(boxdists,i), default_type="v") for i in [0,1]]
 
-        _curve.__init__(self, box1, box2,
+        curve_pt.__init__(self, box1, box2,
                         relangle1=relangle1, relangle2=relangle2,
                         absangle1=absangle1, absangle2=absangle2,
                         absbulge=_topt(absbulge), relbulge=relbulge,
                         boxdists=boxdists_pt)
 
-class arc(_arc):
+class arc(arc_pt):
 
     """an arc is a round connector between the centers of two boxes.
     The constructor gets
@@ -334,13 +376,13 @@ class arc(_arc):
 
         boxdists_pt = [_topt(helper.getitemno(boxdists,i), default_type="v") for i in [0,1]]
 
-        _arc.__init__(self, box1, box2,
+        arc_pt.__init__(self, box1, box2,
                       relangle=relangle,
                       absbulge=_topt(absbulge), relbulge=relbulge,
                       boxdists=boxdists_pt)
 
 
-class twolines(_twolines):
+class twolines(twolines_pt):
 
     """a twolines is a connector consisting of two straight lines.
     The construcor takes a combination of angles and lengths:
@@ -358,7 +400,7 @@ class twolines(_twolines):
 
         boxdists_pt = [_topt(helper.getitemno(boxdists,i), default_type="v") for i in [0,1]]
 
-        _twolines.__init__(self, box1, box2,
+        twolines_pt.__init__(self, box1, box2,
                        absangle1=absangle1, absangle2=absangle2,
                        relangle1=relangle1, relangle2=relangle2,
                        relangleM=relangleM,
