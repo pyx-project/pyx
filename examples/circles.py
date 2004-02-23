3b30ec70f549dcb06ccdd33a7e35009e633dec9f
Joerg Lehmann <joerg@pyx-project.org>
2004-02-23 12:29:56 +0000

    work with new normpath code

--- examples/circles.py
+++ examples/circles.py
@@ -4,12 +4,13 @@ circ1 = path.normpath(path.circle(0, 0, 1)) # you don't really need normpath,
 circ2 = path.normpath(path.circle(1, 1, 1)) # but its better to have it once
                                             # for those operations
 (circ1a, circ1b), (circ2a, circ2b) = circ1.intersect(circ2)
-intersection = (circ1.split([circ1b, circ1a])[1]
-                << circ2.split([circ2a, circ2b])[1]
-                << path.closepath())
-union = (circ1.split([circ1b, circ1a])[0]
-         << circ2.split([circ2a, circ2b])[0]
-         << path.closepath())
+intersection = (circ1.split([circ1a, circ1b])[0]
+                << circ2.split([circ2b, circ2a])[0])
+intersection.append(path.closepath())
+
+union = (circ1.split([circ1a, circ1b])[1]
+         << circ2.split([circ2b, circ2a])[1])
+union.append(path.closepath())
 
 c = canvas.canvas()
 c.fill(union, [color.rgb.blue])
