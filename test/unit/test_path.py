0eeb500d0abbc05cbb4f1d78ddbddae6e7d678b0
Joerg Lehmann <joerg@pyx-project.org>
2004-03-06 14:54:50 +0000

    add normpath.split unit tests

---
+++ test/unit/test_path.py
@@ -0,0 +1,121 @@
+import unittest
+
+from pyx import *
+from pyx.path import *
+
+epsilon = 1e-5
+def isEqual(l1, l2):
+    return abs(unit.topt(l1-l2))<epsilon
+    
+
+
+class NormpathTestCase(unittest.TestCase):
+    def testsplit(self):
+        p = path(moveto(0,0), lineto(1,0), moveto(2,0), lineto(3,0))
+        np = normpath(p)
+
+        # one split parameter
+        sp = np.split([0])
+        assert len(sp)==2 and sp[0] is None and isEqual(sp[1].arclength(), 2)
+        
+        sp = np.split([0.5])
+        assert len(sp)==2 and isEqual(sp[0].arclength(), 0.5) and isEqual(sp[1].arclength(), 1.5)
+        
+        sp = np.split([1])
+        assert len(sp)==2 and isEqual(sp[0].arclength(), 1) and isEqual(sp[1].arclength(), 1)
+        
+        sp = np.split([1.5])
+        assert len(sp)==2 and isEqual(sp[0].arclength(), 1.5) and isEqual(sp[1].arclength(), 0.5)
+        
+        sp = np.split([2])
+        assert len(sp)==2 and isEqual(sp[0].arclength(), 2) and sp[1] is None
+
+        # two split parameters
+        sp = np.split([0, 0.5])
+        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 1.5)
+        
+        sp = np.split([0, 1])
+        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclength(), 1) and isEqual(sp[2].arclength(), 1)
+
+        sp = np.split([0, 1.5])
+        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclength(), 1.5) and isEqual(sp[2].arclength(), 0.5)
+        
+        sp = np.split([0, 2])
+        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclength(), 2) and sp[2] is None
+
+        sp = np.split([0.5, 1])
+        assert len(sp)==3 and isEqual(sp[0].arclength(), 0.5) and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 1)
+
+        sp = np.split([0.5, 1.5])
+        assert len(sp)==3 and isEqual(sp[0].arclength(), 0.5) and isEqual(sp[1].arclength(), 1) and isEqual(sp[2].arclength(), 0.5)
+
+        sp = np.split([0.5, 2])
+        assert len(sp)==3 and isEqual(sp[0].arclength(), 0.5) and isEqual(sp[1].arclength(), 1.5) and sp[2] is None
+
+        sp = np.split([1, 1.5])
+        assert len(sp)==3 and isEqual(sp[0].arclength(), 1) and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 0.5)
+
+        sp = np.split([1, 2])
+        assert len(sp)==3 and isEqual(sp[0].arclength(), 1) and isEqual(sp[1].arclength(), 1) and sp[2] is None
+
+        sp = np.split([1.5, 2])
+        assert len(sp)==3 and isEqual(sp[0].arclength(), 1.5) and isEqual(sp[1].arclength(), 0.5) and sp[2] is None
+        
+        # three split parameters
+        sp = np.split([0, 0.5, 1])
+        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 0.5) and isEqual(sp[3].arclength(), 1)
+
+        sp = np.split([0, 0.5, 1.5])
+        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 1) and isEqual(sp[3].arclength(), 0.5)
+
+        sp = np.split([0, 0.5, 2])
+        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 1.5) and sp[3] is None
+
+        sp = np.split([0, 1, 1.5])
+        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclength(), 1) and isEqual(sp[2].arclength(), 0.5) and isEqual(sp[3].arclength(), 0.5)
+
+        sp = np.split([0, 1, 2])
+        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclength(), 1) and isEqual(sp[2].arclength(), 1) and sp[3] is None
+        
+
+        sp = np.split([0, 1.5, 2])
+        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclength(), 1.5) and isEqual(sp[2].arclength(), 0.5) and sp[3] is None
+
+        sp = np.split([0.5, 1, 1.5])
+        assert len(sp)==4 and isEqual(sp[0].arclength(), 0.5) and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 0.5) and isEqual(sp[3].arclength(), 0.5)
+
+        sp = np.split([0.5, 1.5, 2])
+        assert len(sp)==4 and isEqual(sp[0].arclength(), 0.5) and isEqual(sp[1].arclength(), 1) and isEqual(sp[2].arclength(), 0.5) and sp[3] is None
+
+        sp = np.split([1, 1.5, 2])
+        assert len(sp)==4 and isEqual(sp[0].arclength(), 1) and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 0.5) and sp[3] is None
+
+
+        # four split parameters
+        sp = np.split([0, 0.5, 1, 1.5])
+        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 0.5) and isEqual(sp[3].arclength(), 0.5) and isEqual(sp[4].arclength(), 0.5)
+
+        sp = np.split([0, 0.5, 1, 2])
+        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 0.5) and isEqual(sp[3].arclength(), 1) and sp[4] is None
+
+        sp = np.split([0, 0.5, 1.5, 2])
+        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 1) and isEqual(sp[3].arclength(), 0.5) and sp[4] is None
+
+        sp = np.split([0, 1, 1.5, 2])
+        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclength(), 1) and isEqual(sp[2].arclength(), 0.5) and isEqual(sp[3].arclength(), 0.5) and sp[4] is None
+
+        sp = np.split([0.5, 1, 1.5, 2])
+        assert len(sp)==5 and isEqual(sp[0].arclength(), 0.5) and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 0.5) and isEqual(sp[3].arclength(), 0.5) and sp[4] is None
+
+        
+        # five split parameters
+        sp = np.split([0, 0.5, 1, 1.5, 2])
+        assert len(sp)==6 and sp[0] is None and isEqual(sp[1].arclength(), 0.5) and isEqual(sp[2].arclength(), 0.5) and isEqual(sp[3].arclength(), 0.5) and isEqual(sp[4].arclength(), 0.5) and sp[5] is None
+
+
+suite = unittest.TestSuite((unittest.makeSuite(NormpathTestCase, 'test'),))
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
