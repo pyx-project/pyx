bf2260f26e7734eaf192ac5c2fc7806eedcddc8e
Andre Wobst <wobsta@pyx-project.org>
2004-03-09 15:16:16 +0000

    begin/end line markers for textboxes

--- test/experimental/textboxes.py
+++ test/experimental/textboxes.py
@@ -1,295 +1,63 @@
 import sys; sys.path[:0] = ["../.."]
+import random, string
 from pyx import *
 
 text.set(texipc=1)
 text.set(texdebug="debug.tex", usefiles=["debug.dvi"])
 
+def randpar():
+    return " ".join(["".join([random.choice(string.lowercase)
+                              for j in range(random.randint(1, 3))])
+                     for i in range(random.randint(100, 300))])
 
-t = r"""
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
+def randparwithdisplay():
+    #par = ""
+    par = randpar()
+    for i in range(random.randint(0, 3)):
+        par += r"\display{}xxx\enddisplay{}"
+        for j in range(random.randint(0, 1)):
+            par += randpar()
+    #return par
+    return par + randpar()
 
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
+def randtext():
+    return r"\par{}".join([randparwithdisplay() for i in range(random.randint(1, 10))])
 
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-%
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-%
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-%
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-%
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-%
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text
-
-text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-text text text text text text text text text text text text
-"""
-s = r"""%
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\par
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\par
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\par
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\par
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-\vadjust{\penalty-500}
-\display \hbox{display after pagebreak} A-B\enddisplay
-pg--\the\prevgraf--%
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-\display \hbox{display before pagebreak} A-B\enddisplay
-pg--\the\prevgraf--%
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\par
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-\display A-B\enddisplay
-pg--\the\prevgraf--
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\par
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-par und pagebreak\par
-\display \hbox{display after par} A-B\enddisplay
-pg--\the\prevgraf--%
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\par
-
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\par
-\bigskip
-UND ALLES ZUSAMMEN IN EINER WURST:\par
-\bigskip
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-\display A-B\enddisplay
-pg--\the\prevgraf--%
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-\display A-B\enddisplay
-pg--\the\prevgraf--%
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-\display A-B\enddisplay
-pg--\the\prevgraf--
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-\display A-B\enddisplay
-pg--\the\prevgraf--%
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-Der Rabe Ralf rief schaurig ``Rah! Das End ist nah, das End ist nah!''\
-"""
-
-c = canvas.canvas()
-#shapes = [(8, 14), (11, 8), (8, 14), (11, 8), (8,14)]
-shapes = [(10,7), (8,5)]*10
-boxes = text.defaulttexrunner.textboxes(s, shapes)
-y = 0
-for i in range(len(boxes)):
-    if i < len(shapes):
-        shape = shapes[i]
-    # else shape = shapes[-1] = last shape
+def output(boxes):
+    c = canvas.canvas()
     c.stroke(path.rect(0, y, shape[0], -shape[1]))
     c.stroke(boxes[i].bbox().path(), [trafo.translate(0, y), color.rgb.red])
     c.insert(boxes[i], [trafo.translate(0, y)])
-    y -= shape[1] + 3
-c.writeEPSfile("textboxes")
+    c.writeEPSfile("textboxes")
+
+random.seed(0)
+for n in range(1):
+    print n
+    shapes = [(10,7), (8,5)]*50
+    thistext = randtext()
+    boxes = text.defaulttexrunner.textboxes(thistext, shapes)
+    thistextfile = open("debug.thistext", "w")
+    thistextfile.write(thistext)
+    thistextfile.close()
+    y = 0
+    c = canvas.canvas()
+    for i in range(len(boxes)):
+        if i < len(shapes):
+            shape = shapes[i]
+        #if abs(unit.topt(boxes[i].bbox().right()) - unit.topt(shape[0])) > 1:
+            #print "right boundary differs!"
+            #print unit.topt(boxes[i].bbox().right()), unit.topt(shape[0]), i, len(boxes)
+        c.stroke(path.rect(0, y, shape[0], -shape[1]))
+        c.stroke(boxes[i].bbox().path(), [trafo.translate(0, y), color.rgb.blue])
+        c.insert(boxes[i], [trafo.translate(0, y)])
+        for mark in boxes[i].markers.keys():
+            mx, my = boxes[i].markers[mark]
+            if mark[:5] == "start":
+                c.fill(path.circle(mx, my+y, 0.05), [color.rgb.red])
+            elif mark[:3] == "end":
+                c.fill(path.circle(mx, my+y, 0.05), [color.rgb.green])
+            else:
+                raise "other marks in there!"
+        y -= shape[1] + 3
+    c.writeEPSfile("textboxes")
 
