642bd22cd1ab667d36c9982b61177df179adfdc0
Michael Schindler <m-schindler@users.sourceforge.net>
2004-03-18 13:30:34 +0000

    improved

--- examples/connect.py
+++ examples/connect.py
@@ -5,18 +5,20 @@ unit.set(uscale=3)
 c = canvas.canvas()
 
 textattrs = [text.halign.center, text.vshift.middlezero]
-A = c.text(0, 0, r"\bf A", textattrs)
-B = c.text(1, 0, r"\bf B", textattrs)
-C = c.text(1, 1, r"\bf C", textattrs)
-D = c.text(0, 1, r"\bf D", textattrs)
+A = text.text(0, 0, r"\bf A", textattrs)
+B = text.text(1, 0, r"\bf B", textattrs)
+C = text.text(1, 1, r"\bf C", textattrs)
+D = text.text(0, 1, r"\bf D", textattrs)
 
 for X in [A, B, C, D]:
-    c.stroke(X.bbox().enlarged(0.1).rect())
+    c.draw(X.bbox().enlarged(0.1).path(),
+        [deco.stroked(), deco.filled([color.grey(0.85)])])
+    c.insert(X)
 
 for X,Y in [[A, B], [B, C], [C, D], [D, A]]:
-    c.stroke(connector.arc(X, Y, boxdists=0.25), [color.rgb.red, deco.earrow.normal])
+    c.stroke(connector.arc(X, Y, boxdists=0.2), [color.rgb.red, deco.earrow.normal])
 
-c.stroke(connector.curve(D, B, boxdists=0.25, relangle1=45, relangle2=-45, relbulge=0.8),
+c.stroke(connector.curve(D, B, boxdists=0.2, relangle1=45, relangle2=-45, relbulge=0.8),
          [color.rgb.blue, deco.earrow.normal])
 
 c.writeEPSfile("connect")
