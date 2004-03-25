4db6432f4a555fac3197506ce4a0f31311a41f6d
Andre Wobst <wobsta@pyx-project.org>
2004-03-25 08:49:17 +0000

    some new graph doc

--- manual/graph2.py
+++ manual/graph2.py
@@ -1,5 +1,5 @@
 from pyx import *
-g = graph.graphxy(width=10)
-g.plot(graph.data.function("y=x**2"))
+g = graph.graphxy(width=8)
 g.plot(graph.data.file("graph.dat", x=1, y=2))
+g.plot(graph.data.function("y=x**2"))
 g.writeEPSfile("graph2")
