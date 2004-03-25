df6faa5dfb7e86792cfcc1071a6def18ed134fbe
Andre Wobst <wobsta@pyx-project.org>
2004-03-25 14:36:19 +0000

    new numbering scheme in data.list

--- examples/graphs/mandel.py
+++ examples/graphs/mandel.py
@@ -35,7 +35,7 @@ for re_index in range(gridx):
 g = graph.graphxy(height=8, width=8,
                   x=graph.axis.linear(min=re_min, max=re_max, title=r'$\Re(c)$'),
                   y=graph.axis.linear(min=im_min, max=im_max, title=r'$\Im(c)$'))
-g.plot(graph.data.list(d, xmin=0, xmax=1, ymin=2, ymax=3, color=4),
+g.plot(graph.data.list(d, xmin=1, xmax=2, ymin=3, ymax=4, color=5),
        graph.style.rect(color.palette.Rainbow))
 g.dodata() # plot data first, then axes
 g.writeEPSfile('mandel')
