c3ee4f1b55b18ab9cd0648b552482e15df18206f
Andre Wobst <wobsta@pyx-project.org>
2004-04-15 14:14:19 +0000

    pyx pdf vs. pdf(la)tex comparision example

--- test/experimental/pdftext.py
+++ test/experimental/pdftext.py
@@ -3,7 +3,7 @@ import sys; sys.path.insert(0, "../..")
 from pyx import *
 
 c = canvas.canvas()
-c.text(0, 0, "Hello, world!")
+c.text(0, 0, "$ooooooooooooowo$")
 c.text(0, 1, "$E=mc^2$")
 c.text(0, 2, r"\int\limits_{-\infty}^\infty \!{\rm d}x\, e^{-a x^2} = \sqrt{\pi\over a}", [text.mathmode])
 c.writePDFfile("pdftext")
