cd8aab58973f9488dc57e584e5ffb16c0d285fb2
Andre Wobst <wobsta@pyx-project.org>
2004-07-28 16:52:26 +0000

    vector equations (can be mixed with scalar equations)

--- test/experimental/solve.py
+++ test/experimental/solve.py
@@ -44,14 +44,14 @@ class scalar:
 
     __radd__ = __add__
 
+    def __neg__(self):
+        return term([-1], [self], 0)
+
     def __sub__(self, other):
         return term([1], [self], 0) - other
 
     def __rsub__(self, other):
         return term([-1], [self], 0) + other
-
-    def __neg__(self):
-        return term([-1], [self], 0)
 
     def __mul__(self, other):
         return term([other], [self], 0)
@@ -79,12 +79,71 @@ class scalar:
 
     def __str__(self):
         if self.is_set():
-            return str(self.value)
+            return "%s[=%s]" % (self.varname, self.value)
         else:
             return self.varname
 
     def __float__(self):
         return self.get()
+
+
+class vector(scalar):
+
+    def __init__(self, dimension, varname="(no variable name provided)"):
+        scalar.__init__(self, varname=varname)
+        del self.value # disallow is_set, set, get
+        self.scalars = [scalar(varname="%s%i" % (varname, i)) for i in range(dimension)]
+
+    def __getitem__(self, i):
+        return self.scalars[i]
+
+    def __str__(self):
+        return "%s[=(%s)]" % (self.varname, ",".join([str(scalar) for scalar in self.scalars]))
+
+
+class point:
+
+    def __init__(self, *values):
+        self.values = values
+
+    def term(self):
+        return term([], [], self)
+
+    def __getitem__(self, i):
+        return self.values[i]
+
+    def __add__(self, other):
+        if other is 0: # XXX: is 0 ?!
+            # the default constant in a term is zero, but it should be interpreted as a point(0, ...) as well
+            return self
+        else:
+            try:
+                # other might be a point and we should return a point
+                # (this is the typical case for term.const in a vector equation)
+                return point(*[x + y for x, y in zip(self.values, other.values)])
+            except AttributeError:
+                # otherwise its likely, that the other item is a term and we
+                # should return a term
+                return self.term() + other
+
+    __radd__ = __add__
+
+    def __neg__(self):
+        return point(*[-value for value in self.values])
+
+    def __sub__(self, other):
+        return -other+self
+
+    def __rsub__(self, other):
+        return -self+other
+
+    def __mul__(self, other):
+        return point(*[other*value for value in self.values])
+
+    __rmul__ = __mul__
+
+    def __div__(self, other):
+        return point(*[value/other for value in self.values])
 
 
 class term:
@@ -109,22 +168,24 @@ class term:
             other = term([], [], other)
         vars = self.vars[:]
         prefactors = self.prefactors[:]
-        vids = [v.id for v in vars]
+        vids = [v.id for v in vars] # already existing variable ids
         for p, v in zip(other.prefactors, other.vars):
             try:
+                # try to modify prefactor for existing variables
                 prefactors[vids.index(v.id)] += p
             except ValueError:
+                # or add the variable
                 vars.append(v)
                 prefactors.append(p)
         return term(prefactors, vars, self.const + other.const)
 
     __radd__ = __add__
 
-    def __sub__(self, other):
-        return self + (-other)
-
     def __neg__(self):
         return term([-p for p in self.prefactors], self.vars, -self.const)
+
+    def __sub__(self, other):
+        return -other+self
 
     def __rsub__(self, other):
         return -self+other
@@ -138,7 +199,31 @@ class term:
         return term([p/other for p in self.prefactors], self.vars, self.const/other)
 
     def __eq__(self, other):
-        solver.add(self-other)
+        eq = self - other
+        if not len(eq.vars):
+            raise RuntimeError("equation without variables")
+        try:
+            # is it a vector equation?
+            neqs = len(eq.vars[0].scalars)
+        except AttributeError:
+            for v in eq.vars[1:]:
+                try:
+                    v.scalars
+                except AttributeError:
+                    pass
+                else:
+                    raise RuntimeError("mixed scalar/vector equation")
+            solver.add(eq)
+        else:
+            for v in eq.vars[1:]:
+                if len(v.scalars) != neqs:
+                    raise RuntimeError("vectors of different dimension")
+            if eq.const is 0: # XXX: is 0 ?!
+                eq.const = point(*([0]*neqs))
+            elif len(eq.const.values) != neqs:
+                raise RuntimeError("wrong dimension of constant")
+            for i in range(neqs):
+                solver.add(term(eq.prefactors, [var[i] for var in eq.vars], eq.const[i]))
 
     def __str__(self):
         return "+".join(["%s*%s" % pv for pv in zip(self.prefactors, self.vars)]) + "+" + str(self.const)
@@ -199,8 +284,8 @@ class Solver:
                     i, = [i for i, selfeq in enumerate(self.eqs) if selfeq.id == eq.id]
                     del self.eqs[i]
                 return 1
-            else:
-                assert len(vids) > l
+            elif len(vids) < l:
+                raise RuntimeError("equations are overdetermined")
         return 0
 
 solver = Solver()
@@ -208,16 +293,15 @@ solver = Solver()
 
 if __name__ == "__main__":
 
-    x = scalar("x")
-    y = scalar("y")
-    z = scalar("z")
+    x = vector(2, "x")
+    y = vector(2, "y")
+    z = vector(2, "z")
 
-    x + y == 2*x - y + 3
-    x - y == z
-    5 == z
+    4*x + y == 2*x - y + point(4, 0) # => x + y = (2, 0)
+    x[0] - y[0] == z[1]
+    x[1] - y[1] == z[0]
+    point(5, 0) == z
 
-    print "x=%s" % x
-    print "y=%s" % y
-    print "z=%s" % z
+    print x, y, z
 
 
