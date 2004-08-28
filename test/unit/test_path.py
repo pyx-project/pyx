acc7cd55e60993bbba3432a5fa4c381a2c54fc76
Joerg Lehmann <joerg@pyx-project.org>
2004-08-28 22:42:45 +0000

    more test cases regarding normsubpath.intersect

--- test/unit/test_path.py
+++ test/unit/test_path.py
@@ -14,122 +14,156 @@ def isEqual(l1, l2):
 
 class NormpathTestCase(unittest.TestCase):
     def testsplit(self):
-        p = path(moveto(0,0), lineto(1,0), moveto(2,0), lineto(3,0))
-        np = normpath(p)
+        p = normsubpath([normline(0, -1, 1, 0.1),
+                         normline(1, 0.1, 2, -1),
+                         normline(2, -1, 1, -1), 
+                         normline(1, -1, 1, 1)])
 
-        # one split parameter
-        sp = np.split([0])
-        assert len(sp)==2 and sp[0] is None and isEqual(sp[1].arclen(), 2)
-        
-        sp = np.split([0.5])
-        assert len(sp)==2 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 1.5)
-        
-        sp = np.split([1])
-        assert len(sp)==2 and isEqual(sp[0].arclen(), 1) and isEqual(sp[1].arclen(), 1)
-        
-        sp = np.split([1.5])
-        assert len(sp)==2 and isEqual(sp[0].arclen(), 1.5) and isEqual(sp[1].arclen(), 0.5)
-        
-        sp = np.split([2])
-        assert len(sp)==2 and isEqual(sp[0].arclen(), 2) and sp[1] is None
-
-        # two split parameters
-        sp = np.split([0, 0.5])
-        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1.5)
-        
-        sp = np.split([0, 1])
-        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 1)
-
-        sp = np.split([0, 1.5])
-        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclen(), 1.5) and isEqual(sp[2].arclen(), 0.5)
-        
-        sp = np.split([0, 2])
-        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclen(), 2) and sp[2] is None
-
-        sp = np.split([0.5, 1])
-        assert len(sp)==3 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1)
-
-        sp = np.split([0.5, 1.5])
-        assert len(sp)==3 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 0.5)
-
-        sp = np.split([0.5, 2])
-        assert len(sp)==3 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 1.5) and sp[2] is None
-
-        sp = np.split([1, 1.5])
-        assert len(sp)==3 and isEqual(sp[0].arclen(), 1) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5)
-
-        sp = np.split([1, 2])
-        assert len(sp)==3 and isEqual(sp[0].arclen(), 1) and isEqual(sp[1].arclen(), 1) and sp[2] is None
-
-        sp = np.split([1.5, 2])
-        assert len(sp)==3 and isEqual(sp[0].arclen(), 1.5) and isEqual(sp[1].arclen(), 0.5) and sp[2] is None
-        
-        # three split parameters
-        sp = np.split([0, 0.5, 1])
-        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 1)
-
-        sp = np.split([0, 0.5, 1.5])
-        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1) and isEqual(sp[3].arclen(), 0.5)
-
-        sp = np.split([0, 0.5, 2])
-        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1.5) and sp[3] is None
-
-        sp = np.split([0, 1, 1.5])
-        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5)
-
-        sp = np.split([0, 1, 2])
-        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 1) and sp[3] is None
-        
-
-        sp = np.split([0, 1.5, 2])
-        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 1.5) and isEqual(sp[2].arclen(), 0.5) and sp[3] is None
-
-        sp = np.split([0.5, 1, 1.5])
-        assert len(sp)==4 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5)
-
-        sp = np.split([0.5, 1.5, 2])
-        assert len(sp)==4 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 0.5) and sp[3] is None
-
-        sp = np.split([1, 1.5, 2])
-        assert len(sp)==4 and isEqual(sp[0].arclen(), 1) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and sp[3] is None
+        self.failUnlessEqual(len(p.split([0.9, 1.1, 3.5])), 4)
 
 
-        # four split parameters
-        sp = np.split([0, 0.5, 1, 1.5])
-        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5) and isEqual(sp[4].arclen(), 0.5)
-
-        sp = np.split([0, 0.5, 1, 2])
-        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 1) and sp[4] is None
-
-        sp = np.split([0, 0.5, 1.5, 2])
-        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1) and isEqual(sp[3].arclen(), 0.5) and sp[4] is None
-
-        sp = np.split([0, 1, 1.5, 2])
-        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5) and sp[4] is None
-
-        sp = np.split([0.5, 1, 1.5, 2])
-        assert len(sp)==5 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5) and sp[4] is None
-
-        
-        # five split parameters
-        sp = np.split([0, 0.5, 1, 1.5, 2])
-        assert len(sp)==6 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5) and isEqual(sp[4].arclen(), 0.5) and sp[5] is None
+#        p = path(moveto(0,0), lineto(1,0), moveto(2,0), lineto(3,0))
+#        np = normpath(p)
+#
+#        # one split parameter
+#        sp = np.split([0])
+#        assert len(sp)==2 and sp[0] is None and isEqual(sp[1].arclen(), 2)
+#        
+#        sp = np.split([0.5])
+#        assert len(sp)==2 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 1.5)
+#        
+#        sp = np.split([1])
+#        assert len(sp)==2 and isEqual(sp[0].arclen(), 1) and isEqual(sp[1].arclen(), 1)
+#        
+#        sp = np.split([1.5])
+#        assert len(sp)==2 and isEqual(sp[0].arclen(), 1.5) and isEqual(sp[1].arclen(), 0.5)
+#        
+#        sp = np.split([2])
+#        assert len(sp)==2 and isEqual(sp[0].arclen(), 2) and sp[1] is None
+#
+#        # two split parameters
+#        sp = np.split([0, 0.5])
+#        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1.5)
+#        
+#        sp = np.split([0, 1])
+#        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 1)
+#
+#        sp = np.split([0, 1.5])
+#        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclen(), 1.5) and isEqual(sp[2].arclen(), 0.5)
+#        
+#        sp = np.split([0, 2])
+#        assert len(sp)==3 and sp[0] is None and isEqual(sp[1].arclen(), 2) and sp[2] is None
+#
+#        sp = np.split([0.5, 1])
+#        assert len(sp)==3 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1)
+#
+#        sp = np.split([0.5, 1.5])
+#        assert len(sp)==3 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 0.5)
+#
+#        sp = np.split([0.5, 2])
+#        assert len(sp)==3 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 1.5) and sp[2] is None
+#
+#        sp = np.split([1, 1.5])
+#        assert len(sp)==3 and isEqual(sp[0].arclen(), 1) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5)
+#
+#        sp = np.split([1, 2])
+#        assert len(sp)==3 and isEqual(sp[0].arclen(), 1) and isEqual(sp[1].arclen(), 1) and sp[2] is None
+#
+#        sp = np.split([1.5, 2])
+#        assert len(sp)==3 and isEqual(sp[0].arclen(), 1.5) and isEqual(sp[1].arclen(), 0.5) and sp[2] is None
+#        
+#        # three split parameters
+#        sp = np.split([0, 0.5, 1])
+#        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 1)
+#
+#        sp = np.split([0, 0.5, 1.5])
+#        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1) and isEqual(sp[3].arclen(), 0.5)
+#
+#        sp = np.split([0, 0.5, 2])
+#        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1.5) and sp[3] is None
+#
+#        sp = np.split([0, 1, 1.5])
+#        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5)
+#
+#        sp = np.split([0, 1, 2])
+#        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 1) and sp[3] is None
+#        
+#
+#        sp = np.split([0, 1.5, 2])
+#        assert len(sp)==4 and sp[0] is None and isEqual(sp[1].arclen(), 1.5) and isEqual(sp[2].arclen(), 0.5) and sp[3] is None
+#
+#        sp = np.split([0.5, 1, 1.5])
+#        assert len(sp)==4 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5)
+#
+#        sp = np.split([0.5, 1.5, 2])
+#        assert len(sp)==4 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 0.5) and sp[3] is None
+#
+#        sp = np.split([1, 1.5, 2])
+#        assert len(sp)==4 and isEqual(sp[0].arclen(), 1) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and sp[3] is None
+#
+#
+#        # four split parameters
+#        sp = np.split([0, 0.5, 1, 1.5])
+#        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5) and isEqual(sp[4].arclen(), 0.5)
+#
+#        sp = np.split([0, 0.5, 1, 2])
+#        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 1) and sp[4] is None
+#
+#        sp = np.split([0, 0.5, 1.5, 2])
+#        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 1) and isEqual(sp[3].arclen(), 0.5) and sp[4] is None
+#
+#        sp = np.split([0, 1, 1.5, 2])
+#        assert len(sp)==5 and sp[0] is None and isEqual(sp[1].arclen(), 1) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5) and sp[4] is None
+#
+#        sp = np.split([0.5, 1, 1.5, 2])
+#        assert len(sp)==5 and isEqual(sp[0].arclen(), 0.5) and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5) and sp[4] is None
+#
+#        
+#        # five split parameters
+#        sp = np.split([0, 0.5, 1, 1.5, 2])
+#        assert len(sp)==6 and sp[0] is None and isEqual(sp[1].arclen(), 0.5) and isEqual(sp[2].arclen(), 0.5) and isEqual(sp[3].arclen(), 0.5) and isEqual(sp[4].arclen(), 0.5) and sp[5] is None
 
     def testshortnormsubpath(self):
         sp = normsubpath(epsilon=1)
-        sp.append(normline(0,0, 0.5, 0))
-        sp.append(normline(0.5,0, 1.5, 0))
+        sp.append(normline(0, 0, 0.5, 0))
+        sp.append(normline(0.5, 0, 1.5, 0))
 
-        sp.append(normline(1.5,0, 1.5, 0.3))
-        sp.append(normline(1.5,0.3, 1.5, 0.6))
-        sp.append(normline(1.5,0.6, 1.5, 0.9))
-        sp.append(normline(1.5,0.9, 1.5, 1.2))
+        sp.append(normline(1.5, 0, 1.5, 0.3))
+        sp.append(normline(1.5, 0.3, 1.5, 0.6))
+        sp.append(normline(1.5, 0.6, 1.5, 0.9))
+        sp.append(normline(1.5, 0.9, 1.5, 1.2))
 
         sp.append(normline(1.2, 1.5, 1.3, 1.6))
         sp.append(normcurve(1.3, 1.6, 1.4, 1.7, 1.3, 1.7, 1.3, 1.8))
         sp.append(normcurve(1.3, 1.8, 2.4, 2.7, 3.3, 3.7, 1.4, 1.8))
 
-        self.failUnlessEqual(str(sp), "subpath(open, [normline(0, 0, 1.5, 0), normline(1.5, 0, 1.5, 1.2), normcurve(1.5, 1.2, 2.4, 2.7, 3.3, 3.7, 1.4, 1.8)])")
+        self.failUnlessEqual(str(sp), "subpath(open, [normline(0, 0, 1.5, 0), normline(1.5, 0, 1.5, 1.2), normcurve(1.2, 1.5, 2.4, 2.7, 3.3, 3.7, 1.4, 1.8)])")
+
+    def testintersectnormsubpath(self):
+        p1 = normsubpath([normline(0, 0, 2, 0)], epsilon=1)
+        p2 = normsubpath([normline(0, -1, 1, 0.1),
+                          normline(1, 0.1, 2, -1),
+                          normline(2, -1, 1, -1),
+                          normline(1, -1, 1, 1)], epsilon=1)
+        self.failUnlessEqual(len(p1.intersect(p2)[0]), 2)
+
+        p1 = normsubpath([normline(1, 1, 0, -0.1),
+                          normline(0, -0.1, -1, 1),
+                          normline(-1, 1, 0.1, 1),
+                          normline(0.1, 1, 0.1, -1.5)], epsilon=1)
+        p2 = normsubpath([normline(-1, -1, 0, 0.1),
+                          normline(0, 0.1, 1, -1),
+                          normline(1, -1, -0.1, -1),
+                          normline(-0.1, -1, -0.1, 1.5)], epsilon=1)
+        self.failUnlessEqual(len(p1.intersect(p2)[0]), 5)
+
+        p1 = normsubpath([normline(-1, 0, 1, 0)])
+        p2 = normsubpath([normline(0, 0.1, 0.1, -0.1),
+                          normline(0.1, -0.1, 0, 1),
+                          normline(0, 1, -0.1, -0.1),
+                          normline(-0.1, -0.1, 0, 0.1)], closed=1)
+        p1.epsilon = p2.epsilon = 0.2
+        print p1.intersect(p2)
 
 
 if __name__ == "__main__":
