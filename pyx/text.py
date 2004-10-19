2abc67b4d19e3e73d65e43f4bde442e986d3bfc6
Andre Wobst <wobsta@pyx-project.org>
2004-10-19 12:55:38 +0000

    correct docstring (missed during cut&paste)

--- pyx/text.py
+++ pyx/text.py
@@ -412,7 +412,7 @@ nomathmode = attr.clearclass(_mathmode)
 
 
 class _phantom(attr.attr, textattr, _localattr):
-    "math mode"
+    "phatom text"
 
     def apply(self, expr):
         return r"\phantom{%s}" % expr
