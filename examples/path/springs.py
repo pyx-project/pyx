d3991dd4e676f50a3003180b90625dd9de3bb738
Andre Wobst <wobsta@pyx-project.org>
2004-10-21 08:33:51 +0000

    circular spring example by GLI

---
+++ examples/path/springs.py
@@ -0,0 +1,26 @@
+# contributed by Gerd Ingold
+
+from pyx import *
+from math import cos, sin, pi
+
+n = 3                 # number of masses
+r = 3.0               # system radius
+dphi = 360.0/float(n) # angle between masses
+dphir = dphi/180.0*pi #    the same in radians
+rcyc = 0.3            # radius of cycloid
+nl   = 13             # number of loops
+rc = 0.5              # radius of masses
+eps = 0.03            # extra spacing for surrounding circles
+
+c = canvas.canvas()
+for i in range(n):
+  c.stroke(path.path(path.arc(0, 0, r, i*dphi, (i+1)*dphi)), 
+               [deformer.cycloid(rcyc, nl)])
+  c.fill(path.circle(r*cos(i*dphir), r*sin(i*dphir), rc), 
+               [deco.filled([color.grey.black])])
+
+c.stroke(path.circle(0,0,r - rc - eps))
+c.stroke(path.circle(0,0,r + rc + eps))
+
+c.writeEPSfile("springs")
+
