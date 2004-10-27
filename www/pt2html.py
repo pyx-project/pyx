3754408202089f9638a189540c2452203eadc8ce
Andre Wobst <wobsta@pyx-project.org>
2004-10-27 15:06:10 +0000

    updates to the html build system

--- www/pt2html.py
+++ www/pt2html.py
@@ -1,44 +1,89 @@
 #!/usr/bin/env python
-# -*- coding: ISO-8859-1 -*-
 
-import sys, os, os.path
-from zope.pagetemplate.pagetemplate import PageTemplate
+import sys, os, os.path, cgi, StringIO, codecs, glob
+import keyword, token, tokenize
+from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+import Image
 
 sys.path[:0]=[".."]
 import pyx
 
+_KEYWORD = token.NT_OFFSET
+
+tokclasses = {token.NUMBER: 'number',
+              token.OP: 'op',
+              token.STRING: 'string',
+              tokenize.COMMENT: 'comment',
+              token.NAME: 'name',
+              _KEYWORD: 'keyword'}
+
+class py2html:
+
+    def __init__(self, input, output):
+        self.output = output
+        self.col = 0
+        self.tokclass = None
+        self.output.write("<pre id=python>")
+        tokenize.tokenize(input.readline, self.tokeneater)
+        if self.tokclass is not None:
+            self.output.write('</span>')
+        self.output.write("</pre>\n")
+
+    def tokeneater(self, toktype, toktext, (srow, scol), (erow, ecol), line):
+        if toktype == token.ERRORTOKEN:
+            raise RuntimeError("ErrorToken occured")
+        if toktype in [token.NEWLINE, tokenize.NL]:
+            self.output.write('\n')
+            self.col = 0
+        else:
+            # map token type to a color group
+            if token.LPAR <= toktype and toktype <= token.OP:
+                toktype = token.OP
+            elif toktype == token.NAME and keyword.iskeyword(toktext):
+                toktype = _KEYWORD
+
+            # restore whitespace
+            assert scol >= self.col
+            self.output.write(" "*(scol-self.col))
+
+            try:
+                tokclass = tokclasses[toktype]
+            except KeyError:
+                tokclass = None
+            if self.tokclass is not None and tokclass != self.tokclass:
+                self.output.write('</span>')
+            if tokclass is not None:
+                self.output.write('<span class="%s">' % tokclass)
+            self.output.write(cgi.escape(toktext))
+            self.tokclass = tokclass
+
+            # calculate new column position
+            self.col = scol + len(toktext)
+            newline = toktext.rfind("\n")
+            if newline != -1:
+                self.col = len(toktext) - newline - 1
+
 class example:
+
     def __init__(self, name):
+        if name.startswith("./"):
+            name = name[2:]
         self.name = name
         self.png = "%s.png" % os.path.basename(self.name)
         self.eps = "%s.eps" % os.path.basename(self.name)
-        self.code = open("../examples/%s.py.html" % name, "r").read()
-        self.code = self.code.replace("ä", "&auml;")
-        self.code = self.code.replace("Ä", "&Auml;")
-        self.code = self.code.replace("ö", "&ouml;")
-        self.code = self.code.replace("Ö", "&Ouml;")
-        self.code = self.code.replace("ü", "&uuml;")
-        self.code = self.code.replace("Ü", "&Uuml;")
-        self.code = self.code.replace("ß", "&szlig;")
-        self.code = self.code.replace("é", "&eacute;")
-    def __getattr__(self, attr):
-        return self.__dict__[attr]
+        htmlbuffer = StringIO.StringIO()
+        self.width, self.height = Image.open("../examples/%s.png" % name).size
+        py2html(codecs.open("../examples/%s.py" % name, encoding="iso-8859-1"), htmlbuffer)
+        self.code = htmlbuffer.getvalue()
 
-def PageTemplateFromFile(filename):
-    pt = PageTemplate()
-    pt.write(open(filename, "r").read())
-    return pt
 
-def write_file(filename, string):
-    try:
-        open(filename, "w").write(string)
-    except IOError:
-        os.mkdir(os.path.dirname(filename)) # do not need to create directory recursively when called in proper order
-        open(filename, "w").write(string)
+class MyPageTemplateFile(PageTemplateFile):
 
-maintemplate = PageTemplateFromFile("maintemplate.pt")
+    def write(self, text):
+        if isinstance(text, str):
+            text = unicode(text, encoding="iso-8859-1")
+        return PageTemplateFile.write(self, text)
 
-htmlname = sys.argv[1]
 
 def mkrellink(linkname, options):
     # returns a string containing the relative url for linkname (an absolute url)
@@ -55,24 +100,40 @@ def mkrellink(linkname, options):
         linkname = "../" + linkname
     return linkname
 
-if htmlname.startswith("examples"):
-    dir = htmlname[9:-10]
-    examplepages = [item[:-2] for item in open("../examples/INDEX").readlines() if item[-2] == "/"]
+maintemplate = MyPageTemplateFile("maintemplate.pt")
+
+for ptname in glob.glob("*.pt"):
+    if ptname in ["maintemplate.pt", "examples.pt"]:
+        continue
+    htmlname = "%s.html" % ptname[:-3]
+    print htmlname
+    template = MyPageTemplateFile(ptname)
+    content = template(pagename=htmlname,
+                       maintemplate=maintemplate,
+                       examplepages=[],
+                       mkrellink=mkrellink,
+                       version=pyx.__version__)
+    codecs.open("build/%s" % htmlname, "w", encoding="iso-8859-1").write(content)
+
+examplestemplate = MyPageTemplateFile("examples.pt")
+examplepages = [item[:-2] for item in open("../examples/INDEX").readlines() if item[-2] == "/"]
+
+for dir in ["."] + examplepages:
     try:
-        abstract = open("../examples/%sREADME" % dir).read().replace("__version__", pyx.__version__).replace("\PyX{}", "PyX")
+        abstract = open("../examples/%s/README" % dir).read().replace("__version__", pyx.__version__).replace("\PyX{}", "PyX")
     except IOError:
         abstract = ""
-    examples = [example(dir + item.strip()) for item in open("../examples/%sINDEX" % dir).readlines() if item[-2] != "/"]
-    write_file("build/%s" % htmlname, PageTemplateFromFile("examples.pt")(pagename=htmlname,
-                                                                          maintemplate=maintemplate,
-                                                                          abstract=abstract,
-                                                                          examples=examples,
-                                                                          examplepages=examplepages,
-                                                                          mkrellink=mkrellink))
-else:
-    write_file("build/%s" % htmlname, PageTemplateFromFile("%s.pt" % htmlname[:-5])(pagename=htmlname,
-                                                                                    maintemplate=maintemplate,
-                                                                                    examplepages=[],
-                                                                                    mkrellink=mkrellink,
-                                                                                    version=pyx.__version__))
+    examples = [example(dir + "/" + item.strip()) for item in open("../examples/%s/INDEX" % dir).readlines() if item[-2] != "/"]
+    if dir != ".":
+        htmlname = "examples/%s/index.html" % dir
+    else:
+        htmlname = "examples/index.html"
+    print htmlname
+    content = examplestemplate(pagename=htmlname,
+                               maintemplate=maintemplate,
+                               abstract=abstract,
+                               examples=examples,
+                               examplepages=examplepages,
+                               mkrellink=mkrellink)
+    codecs.open("build/%s" % htmlname, "w", encoding="iso-8859-1").write(content)
 
