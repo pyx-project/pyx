9372a561a3b7dec6d8745a0e5dbc1b06657b0026
Andre Wobst <wobsta@pyx-project.org>
2005-01-30 07:55:46 +0000

    a changing symbol style (size and color)

---
+++ examples/graphs/changesymbol.py
@@ -0,0 +1,39 @@
+import random
+from pyx import *
+
+# introduce a new symbol style where size and color are given by some data
+class changesymbol(graph.style.symbol):
+
+    def __init__(self, sizename="size", colorname="color",
+                       palette=color.palette.Rainbow, **kwargs):
+        # add some configuration parameters
+        self.sizename = sizename
+        self.colorname = colorname
+        self.palette = palette
+        graph.style.symbol.__init__(self, **kwargs)
+
+    def columns(self, privatedata, sharedata, agraph, columns):
+        # register the new column names
+        if self.sizename not in columns:
+            raise ValueError("column '%s' missing" % self.sizename)
+        if self.colorname not in columns:
+            raise ValueError("column '%s' missing" % self.colorname)
+        return ([self.sizename, self.colorname] +
+                graph.style.symbol.columns(self, privatedata,
+                                           sharedata, agraph, columns))
+
+    def drawpoint(self, privatedata, sharedata, agraph):
+        # replace the original drawpoint method by a slightly revised one
+        if sharedata.vposvalid and privatedata.symbolattrs is not None:
+            xpos, ypos = agraph.vpos_pt(*sharedata.vpos)
+            color = self.palette.getcolor(sharedata.point[self.colorname])
+            privatedata.symbol(privatedata.symbolcanvas, xpos, ypos,
+                               privatedata.size_pt*sharedata.point[self.sizename],
+                               privatedata.symbolattrs + [color])
+
+g = graph.graphxy(width=10)
+g.plot(graph.data.list([[random.random() for i in range(4)]
+                        for i in range(1000)],
+                       x=1, y=2, size=3, color=4),
+       [changesymbol()])
+g.writeEPSfile("changesymbol")
