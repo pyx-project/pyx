4275e096c1b56bd43ac0a6d843bacf5666a9a7a1
Joerg Lehmann <joerg@pyx-project.org>
2005-06-27 08:17:15 +0000

    currently non-working case of twice used patterns

--- test/functional/test_pattern.py
+++ test/functional/test_pattern.py
@@ -10,7 +10,9 @@ pattern.set([style.linewidth.thin])
 pattern.stroke(path.line(0, 0, 0.25, 0.25))
 pattern.stroke(path.line(0, 0.25, 0.25, 0))
 
+#c.draw(path.circle(3, 3, 3), [deco.filled([pattern])])
 c.draw(path.circle(3, 3, 3), [deco.filled([pattern]), deco.stroked])
+c.draw(path.circle(5, 1, 1), [deco.filled([pattern]), deco.stroked])
 
 pyxpattern = canvas.pattern()
 pyxpattern.text(0.125, 0.125, r"\PyX")
