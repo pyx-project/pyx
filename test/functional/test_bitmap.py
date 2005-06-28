a50d8f41d7785ece928edbf2f5029b51caab0132
Joerg Lehmann <joerg@pyx-project.org>
2005-06-28 08:11:40 +0000

    use new signature of bitmap class

--- test/functional/test_bitmap.py
+++ test/functional/test_bitmap.py
@@ -7,11 +7,11 @@ image_rgb = bitmap.image(3, 2, "RGB", "\77\77\77\177\177\177\277\277\277"
 bitmap_bw_stream = bitmap.bitmap(0, 1, image_bw, height=0.8)
 bitmap_rgb_stream = bitmap.bitmap(0, 0, image_rgb, height=0.8)
 
-bitmap_bw_storestring = bitmap.bitmap(2, 1, image_bw, height=0.8, storedata=1)
-bitmap_rgb_storestring = bitmap.bitmap(2, 0, image_rgb, height=0.8, storedata=1)
+bitmap_bw_storestring = bitmap.bitmap(2, 1, image_bw, height=0.8, PSstoreimage=1)
+bitmap_rgb_storestring = bitmap.bitmap(2, 0, image_rgb, height=0.8, PSstoreimage=1)
 
-bitmap_bw_storearray = bitmap.bitmap(4, 1, image_bw, height=0.8, storedata=1, maxstrlen=2)
-bitmap_rgb_storearray = bitmap.bitmap(4, 0, image_rgb, height=0.8, storedata=1, maxstrlen=2)
+bitmap_bw_storearray = bitmap.bitmap(4, 1, image_bw, height=0.8, PSstoreimage=1, PSmaxstrlen=2)
+bitmap_rgb_storearray = bitmap.bitmap(4, 0, image_rgb, height=0.8, PSstoreimage=1, PSmaxstrlen=2)
 
 c = canvas.canvas()
 c.insert(bitmap_bw_stream)
