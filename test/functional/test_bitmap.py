3241d368df92842e28a4b21e6292ecab6151937b
Andre Wobst <wobsta@pyx-project.org>
2005-06-28 08:21:45 +0000

    updated example with palette example (when PIL is available)

--- test/functional/test_bitmap.py
+++ test/functional/test_bitmap.py
@@ -1,9 +1,17 @@
 import sys; sys.path.insert(0, "../..")
 from pyx import *
+try:
+    from Image import open
+except:
+    print "PIL not available, skipping palette test"
+    paletteimage = None
+else:
+    paletteimage = open("../../www/icons/vcss.png")
 
 image_bw = bitmap.image(2, 2, "L", "\0\377\377\0")
 image_rgb = bitmap.image(3, 2, "RGB", "\77\77\77\177\177\177\277\277\277"
                                       "\377\0\0\0\377\0\0\0\377")
+
 bitmap_bw_stream = bitmap.bitmap(0, 1, image_bw, height=0.8)
 bitmap_rgb_stream = bitmap.bitmap(0, 0, image_rgb, height=0.8)
 
@@ -16,16 +24,12 @@ bitmap_rgb_storearray = bitmap.bitmap(4, 0, image_rgb, height=0.8, PSstoreimage=
 c = canvas.canvas()
 c.insert(bitmap_bw_stream)
 c.insert(bitmap_rgb_stream)
-c.insert(bitmap_bw_stream)
-c.insert(bitmap_rgb_stream)
-c.insert(bitmap_bw_storestring)
-c.insert(bitmap_rgb_storestring)
 c.insert(bitmap_bw_storestring)
 c.insert(bitmap_rgb_storestring)
 c.insert(bitmap_bw_storearray)
 c.insert(bitmap_rgb_storearray)
-c.insert(bitmap_bw_storearray)
-c.insert(bitmap_rgb_storearray)
+if paletteimage:
+    c.insert(bitmap.bitmap(6, 0, paletteimage, height=1.8))
 c.writeEPSfile("test_bitmap", paperformat=document.paperformat.A4)
 c.writePDFfile("test_bitmap", paperformat=document.paperformat.A4)
 
