d539a1ac6ee54b80e51ee91366e4b535010b7395
Joerg Lehmann <joerg@pyx-project.org>
2005-07-06 20:17:08 +0000

    updated address of FSF

--- pyx/canvas.py
+++ pyx/canvas.py
@@ -19,7 +19,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PyX; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 """The canvas module provides a PostScript canvas class and related classes
 
--- pyx/t1strip/fullfont.py
+++ pyx/t1strip/fullfont.py
@@ -18,7 +18,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PyX; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 _PFB_ASCII = "\200\1"
 _PFB_BIN = "\200\2"
