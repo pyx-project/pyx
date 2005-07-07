9bb78e7ad8cc022b9cf28b2681b3ebf86db1ca78
Andre Wobst <wobsta@pyx-project.org>
2005-07-07 13:51:18 +0000

    test empty page

--- test/functional/test_document.py
+++ test/functional/test_document.py
@@ -6,7 +6,8 @@ d = document.document()
 
 for nr in range(1, 10):
      c = canvas.canvas()
-     c.text(0, 0, "page %d" % nr)
+     if nr != 7:
+         c.text(0, 0, "page %d" % nr)
      d.append(document.page(c, pagename=chr(64+nr), rotated=(nr-1)%2, fittosize=1, paperformat=document.paperformat.A4))
 
 d.writePSfile("test_document")
