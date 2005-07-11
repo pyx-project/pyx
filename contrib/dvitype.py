8b33316fd08d8c1ffa51405b3a868225470cc51f
Andre Wobst <wobsta@pyx-project.org>
2005-07-11 09:36:48 +0000

    streamline dvitype.py

--- contrib/dvitype.py
+++ contrib/dvitype.py
@@ -1,13 +1,31 @@
-# This is a dvitype clone based on the dvi reader of PyX.
-# 
-# Note that its absolutely minimalistic and it misses some
-# features of dvitype. Its just a a gimmick ...
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+#
+#
+# Copyright (C) 2005 André Wobst <wobsta@users.sourceforge.net>
+#
+# epstopng is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# epstopng is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with epstopng; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import sys
-from pyx import dvifile
+from optparse import OptionParser
+from pyx import dvifile, version
 
-df = dvifile.dvifile(sys.argv[1], dvifile.readfontmap(["psfonts.map"]), 1)
+parser = OptionParser(usage="usage: %prog dvi-file", version="%prog " + version.version)
+(options, args) = parser.parse_args()
+if len(args) != 1:
+    parser.error("can process a single dvi-file only")
 
+df = dvifile.dvifile(args[0], dvifile.readfontmap(["psfonts.map"]), 1)
 while df.readpage():
     pass
-
