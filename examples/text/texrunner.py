a9b7e85a5097d48719296422caebbbfc057e592b
Joerg Lehmann <joerg@pyx-project.org>
2005-07-13 08:12:36 +0000

    add a clarifying comment

--- examples/text/texrunner.py
+++ examples/text/texrunner.py
@@ -11,6 +11,8 @@ c = canvas.canvas()
 # This can be changed by the canvas method settexrunner.
 c.text(0, 0, r"This is \LaTeX.")
 
+# If you want to use another texrunner temporarily, you can just insert 
+# a text box manually
 plaintex = text.texrunner() # plain TeX instance
 c.insert(plaintex.text(0, -1, r"This is plain \TeX."))
 
