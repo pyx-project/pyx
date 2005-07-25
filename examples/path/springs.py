16db6b4d03000f5e53ab6ae3658dca9e9eab2d59
Andre Wobst <wobsta@pyx-project.org>
2005-07-25 06:08:39 +0000

    use path methods

--- examples/path/springs.py
+++ examples/path/springs.py
@@ -1,23 +1,24 @@
 # contributed by Gert-Ludwig Ingold
 
 from pyx import *
-from math import cos, sin, pi
 
 n = 3                 # number of masses
 r = 3.0               # system radius
-dphi = 360.0/float(n) # angle between masses
-dphir = dphi/180.0*pi #    the same in radians
 rcyc = 0.3            # radius of cycloid
 nl = 13               # number of loops
 rc = 0.5              # radius of masses
 eps = 0.03            # extra spacing for surrounding circles
 
 c = canvas.canvas()
-for i in range(n):
-  c.stroke(path.path(path.arc(0, 0, r, i*dphi, (i+1)*dphi)), 
-               [deformer.cycloid(rcyc, nl), deformer.smoothed(radius=0.1)])
-  c.fill(path.circle(r*cos(i*dphir), r*sin(i*dphir), rc), 
-               [deco.filled([color.grey.black])])
+springcircle = path.circle(0, 0, r)
+masspositions = [i*springcircle.arclen()/n
+                 for i in range(n)]
+for springsegment in springcircle.split(masspositions):
+    c.stroke(springsegment,
+             [deformer.cycloid(rcyc, nl),
+              deformer.smoothed(radius=0.1)])
+for x, y in springcircle.at(masspositions):
+    c.fill(path.circle(x, y, rc))
 
 c.stroke(path.circle(0, 0, r - rc - eps))
 c.stroke(path.circle(0, 0, r + rc + eps))
