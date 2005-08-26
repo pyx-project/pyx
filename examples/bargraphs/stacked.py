82f92a17ddd5ef3077164be88e7085f6d13eab42
Andre Wobst <wobsta@pyx-project.org>
2005-08-26 16:44:35 +0000

    widen the graph to prevent overprints

--- examples/bargraphs/stacked.py
+++ examples/bargraphs/stacked.py
@@ -8,7 +8,7 @@
 
 from pyx import *
 
-g = graph.graphxy(width=8, x=graph.axis.bar())
+g = graph.graphxy(width=14, height=6, x=graph.axis.bar())
 g.plot(graph.data.file("stacked.dat", xname=0, y=2, stack=3),
        [graph.style.bar(),
         graph.style.text("y"),
