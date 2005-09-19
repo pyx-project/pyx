5bd49a25cd9eca9fd0451916a346f1f84d1df858
Andre Wobst <wobsta@pyx-project.org>
2005-09-19 05:43:12 +0000

    use textattr names instead of hello world

--- examples/text/halign.py
+++ examples/text/halign.py
@@ -1,31 +1,29 @@
-# The horizontal alignment of text distinguishes between the
-# alignment of the box and the alignment of the material within
-# the box (when there are several lines, i.e. we have a parbox).
-# The halign.left/center/right attributes combine both alignments.
-
 from pyx import *
-
-# you can use it in TeX and LaTeX ...
-# text.set(mode="latex")
 
 c = canvas.canvas()
 
 c.set([style.linestyle.dotted])
-c.stroke(path.line(-5, 0, -5, 4))
-c.stroke(path.line(0, 0, 0, 4))
-c.stroke(path.line(5, 0, 5, 4))
+c.stroke(path.line(-5, 0, -5, 5))
+c.stroke(path.line(0, 0, 0, 5))
+c.stroke(path.line(5, 0, 5, 5))
 
-c.text(-5, 4, r"Hello, world!", [text.halign.boxleft])
-c.text(0, 4, r"Hello, world!", [text.halign.boxcenter])
-c.text(5, 4, r"Hello, world!", [text.halign.boxright])
+c.text(-5, 5, r"boxleft", [text.halign.boxleft])
+c.text(0, 5, r"boxcenter", [text.halign.boxcenter])
+c.text(5, 5, r"boxright", [text.halign.boxright])
 
-c.text(0, 3, r"Hello,\par world!", [text.parbox(2.5), text.halign.boxcenter, text.halign.flushleft])
-c.text(0, 2, r"Hello,\par world!", [text.parbox(2.5), text.halign.boxcenter, text.halign.flushcenter])
-c.text(0, 1, r"Hello,\par world!", [text.parbox(2.5), text.halign.boxcenter, text.halign.flushright])
+c.text(0, 4, r"boxcenter and flushleft",
+       [text.parbox(3), text.halign.boxcenter, text.halign.flushleft])
+c.text(0, 3, r"boxcenter and flushcenter",
+       [text.parbox(3), text.halign.boxcenter, text.halign.flushcenter])
+c.text(0, 2, r"boxcenter and flushright",
+       [text.parbox(3), text.halign.boxcenter, text.halign.flushright])
 
-c.text(-5, 0, r"Hello,\par world!", [text.parbox(2.5), text.halign.left])
-c.text(0, 0, r"Hello,\par world!", [text.parbox(2.5), text.halign.center])
-c.text(5, 0, r"Hello,\par world!", [text.parbox(2.5), text.halign.right])
+c.text(-5, 0, r"left: boxleft and flushleft",
+       [text.parbox(3), text.halign.left])
+c.text(0, 0, r"center: boxcenter and flushcenter",
+       [text.parbox(3), text.halign.center])
+c.text(5, 0, r"right: boxright and flushright",
+       [text.parbox(3), text.halign.right])
 
 c.writeEPSfile("halign")
 c.writePDFfile("halign")
