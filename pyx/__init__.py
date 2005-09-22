452fd88c81130c2bf860a1dc9f5dc6091054f85c
Joerg Lehmann <joerg@pyx-project.org>
2005-09-22 07:53:49 +0000

    add new project description in order to make pydoc happy

--- pyx/__init__.py
+++ pyx/__init__.py
@@ -21,6 +21,13 @@
 # along with PyX; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
+"""Python graphics package
+
+PyX is a Python package for the creation of PostScript and PDF files. It
+combines an abstraction of the PostScript drawing model with a TeX/LaTeX
+interface. Complex tasks like 2d and 3d plots in publication-ready quality are
+built out of these primitives.
+"""
 
 import version
 __version__ = version.version
@@ -32,4 +39,3 @@ __all__ = ["attr", "box", "bitmap", "canvas", "color", "connector", "deco", "def
 # automatically import main modules into pyx namespace
 for module in __all__:
     __import__(module, globals(), locals(), [])
-
