6d606f3957bab9c1c9cd41ee6d222fa0b35e16f9
Andre Wobst <wobsta@pyx-project.org>
2006-02-10 18:55:32 +0000

    stable test with constructed polynoms

--- test/unit/test_mathutils.py
+++ test/unit/test_mathutils.py
@@ -2,55 +2,90 @@ import sys
 if sys.path[0] != "../..":
     sys.path.insert(0, "../..")
 
-import unittest, random
-# its certainly not a good practice to use random numbers, but its much better than nothing ...
-# note that the checks might fail in very rare cases, but we've stabelized it quite a bit
-# TODO: make a real testsuite (with known solutions)
+import unittest
 
 from pyx import mathutils
 
 class PolynomTestCase(unittest.TestCase):
 
-    maxOrder = 4
-    triesPerOrder = 100
+    def makePolyRoots(self, *rs):
+        # creates a polynom with zeros rs
+        if len(rs) == 1:
+            return [1, -rs[0]]
+        cs = self.makePolyRoots(*rs[1:])
+        return [cim1-rs[0]*ci for cim1, ci in zip(cs+[0], [0]+cs)]
 
-    def polyValue(self, x, cs):
-        if len(cs) == 0:
-            return 0
-        return x * self.polyValue(x, cs[:-1]) + cs[-1]
+    def makePolyNonroots(self, cs, a, b):
+        # adds a factor ((x-a)**2 + b) to the polynom with coefficients cs
+        # which can be used (for b > 0) to construct polynoms with complex roots
+        return [cim2-2*a*cim1+(a*a+b)*ci for cim2, cim1, ci in zip(cs+[0, 0], [0]+cs+[0], [0, 0]+cs)]
 
-    def derivativeValue(self, x, cs):
-        cs = [(len(cs)-i-1)*c for i, c in enumerate(cs[:-1])]
-        # note that we do not really return the derivative
-        # (we only use it to stablelize the root check)
-        return max(abs(self.polyValue(x, cs)), 1)
+    def compareRoots(self, found, should):
+        found.sort()
+        # we remove degeneracies in found, since we do *not* claim to properly handle degeneracies
+        i = 1
+        while len(found) > i:
+            if found[i] - found[i-1] < 1e-7:
+                del found[i]
+            else:
+                i += 1
+        self.failUnlessEqual(len(found), len(should))
+        for r1, r2 in zip(found, should):
+            self.failUnlessAlmostEqual(r1, r2)
 
-    def testRandom(self):
-        r = []
-        for order in range(self.maxOrder+1):
-            maxroot1 = maxroot2 = maxdist = 0
-            for count in range(self.triesPerOrder):
-                cs = [100*random.random()-50 for i in range(order+1)]
-                if count < 10:
-                    # check for proper handling of highest coefficient equals 0
-                    cs[0] = 0
-                roots1 = mathutils.realpolyroots(*cs)
-                # different from PyX itself this test will also use
-                # realpolyroot_eigenvalue, which needs the numeric package
-                roots2 = mathutils.realpolyroots_eigenvalue(*cs)
-                # check for roots being roots (such a test is in general very
-                # problematic, but the division by the derivative makes it
-                # reasonably stable)
-                for root in roots1 + roots2:
-                    self.failUnlessAlmostEqual(0, self.polyValue(root, cs)/self.derivativeValue(root, cs), 5)
-                roots1.sort()
-                roots2.sort()
-                # compare the two ways of calculating roots
-                # (for random numbers degenerate cases are unlikely;
-                # however, they could generate problems in this simplistic check)
-                self.failUnlessEqual(len(roots1), len(roots2))
-                for root1, root2 in zip(roots1, roots2):
-                    self.failUnlessAlmostEqual(root1, root2, 5)
+    def testConstant(self):
+        self.compareRoots(mathutils.realpolyroots(1), [])
+        self.compareRoots(mathutils.realpolyroots(0), [0])
+
+    def testLinear(self):
+        self.compareRoots(mathutils.realpolyroots(0, 1), [])
+        self.compareRoots(mathutils.realpolyroots(0, 0), [0])
+        self.compareRoots(mathutils.realpolyroots(1, 1), [-1])
+        self.compareRoots(mathutils.realpolyroots(2, 0), [0])
+        self.compareRoots(mathutils.realpolyroots(4, -1), [0.25])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7)), [1.7])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(-42)), [-42])
+
+    def testQuadratic(self):
+        self.compareRoots(mathutils.realpolyroots(0, 1, 1), [-1])
+        self.compareRoots(mathutils.realpolyroots(1, -4, 3), [1, 3])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, 2.3)), [1.7, 2.3])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(2.3, 1.7)), [1.7, 2.3])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, -2.3)), [-2.3, 1.7])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(-2.3, 1.7)), [-2.3, 1.7])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, 1.7)), [1.7])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots([1], 1.7, 1)), [])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots([1], 1.7, -1)), [0.7, 2.7])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots([1], 1.7, -4)), [-0.3, 3.7])
+
+    def testCubic(self):
+        self.compareRoots(mathutils.realpolyroots(0, 0, 0, 1), [])
+        self.compareRoots(mathutils.realpolyroots(0, 0, 1, 1), [-1])
+        self.compareRoots(mathutils.realpolyroots(0, 1, -4, 3), [1, 3])
+        self.compareRoots(mathutils.realpolyroots(1, -9, 23, -15), [1, 3, 5])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, 2.3, 4.2)), [1.7, 2.3, 4.2])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, -2.3, 4.2)), [-2.3, 1.7, 4.2])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, -2.3, -4.2)), [-4.2, -2.3, 1.7])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyRoots(1.7), 2.3, 1)), [1.7])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyRoots(1.7), 2.3, -1)), [1.3, 1.7, 3.3])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyRoots(1.7), 2.3, -4)), [0.3, 1.7, 4.3])
+
+    def testQuartic(self):
+        self.compareRoots(mathutils.realpolyroots(0, 0, 0, 0, 1), [])
+        self.compareRoots(mathutils.realpolyroots(0, 0, 0, 1, 1), [-1])
+        self.compareRoots(mathutils.realpolyroots(0, 1, -9, 23, -15), [1, 3, 5])
+        self.compareRoots(mathutils.realpolyroots(1, -16, 86, -176, 105), [1, 3, 5, 7])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, 2.3, 4.2, 13)), [1.7, 2.3, 4.2, 13])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, -2.3, 4.2, 13)), [-2.3, 1.7, 4.2, 13])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyRoots(1.7, -2.3, -4.2, 13)), [-4.2, -2.3, 1.7, 13])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyRoots(1.7, 2.3), 4.2, 1)), [1.7, 2.3])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyRoots(1.7, 2.3), 4.2, -1)), [1.7, 2.3, 3.2, 5.2])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyRoots(1.7, 2.3), 4.2, -4)), [1.7, 2.2, 2.3, 6.2])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyRoots(1.7, -2.3), 4.2, -1)), [-2.3, 1.7, 3.2, 5.2])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyRoots(1.7, -2.3), 4.2, -4)), [-2.3, 1.7, 2.2, 6.2])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyNonroots([1], 1.7, 1), 2.3, 1)), [])
+        self.compareRoots(mathutils.realpolyroots(*self.makePolyNonroots(self.makePolyNonroots([1], 1.7, 1), 2.3, -1)), [1.3, 3.3])
+
 
 if __name__ == "__main__":
     unittest.main()
