5e3ac73ac727c4cca422a5e3ad2bd64fdc73d857
Michael Schindler <m-schindler@users.sourceforge.net>
2006-05-17 10:28:37 +0000

    arggh, color instead of colour

--- examples/text/color.py
+++ examples/text/color.py
@@ -8,7 +8,7 @@ text.preamble("\usepackage[pyx]{color}")
 text.preamble(r"\definecolor{COL}{cmyk}{%(c)g,%(m)g,%(y)g,%(k)g}" % col.color)
 
 c = canvas.canvas()
-c.text(0, 0, r"\textcolor{COL}{text} and outline have the same colour")
+c.text(0, 0, r"\textcolor{COL}{Text} and outline have the same color")
 c.stroke(path.rect(-0.2, -0.2, 6.2, 0.6), [col])
 c.writeEPSfile("color")
 c.writePDFfile("color")
