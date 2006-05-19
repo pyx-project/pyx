5ff1aac7667a397866e9a84949ac68836cf2c115
Joerg Lehmann <joerg@pyx-project.org>
2006-05-19 11:10:41 +0000

    show difference between trafo as deformer and as canvas trafo

--- examples/drawing2/ellipse.py
+++ examples/drawing2/ellipse.py
@@ -1,8 +1,16 @@
 from pyx import *
 
 c = canvas.canvas()
-c.stroke(path.circle(0, 0, 1), [trafo.scale(sx=2, sy=1.5),
-                                trafo.rotate(45),
-                                trafo.translate(1, 0)])
+circ = path.circle(0, 0, 1)
+
+# variant 1: use trafo as a deformer
+c.stroke(circ, [style.linewidth.THIck, 
+                trafo.scale(sx=2, sy=0.9), trafo.rotate(45), trafo.translate(1, 0)])
+
+# variant 2: transform a subcanvas
+sc = canvas.canvas()
+sc.stroke(circ, [style.linewidth.THIck])
+c.insert(sc, [trafo.scale(sx=2, sy=0.9), trafo.rotate(45), trafo.translate(5, 0)])
+
 c.writeEPSfile("ellipse")
 c.writePDFfile("ellipse")
