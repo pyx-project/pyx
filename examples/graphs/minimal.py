cd51de270ae5041aff4f45d1f4190c538fa8bb6d
Joerg Lehmann <joerg@pyx-project.org>
2006-05-21 15:16:32 +0000

    more graphs example work

--- examples/graphs/minimal.py
+++ examples/graphs/minimal.py
@@ -4,14 +4,3 @@ g = graph.graphxy(width=8)
 g.plot(graph.data.file("minimal.dat", x=1, y=2))
 g.writeEPSfile("minimal")
 g.writePDFfile("minimal")
-
-# the file minimal.dat looks like:
-# 1  2
-# 2  3
-# 3  8
-# 4  13
-# 5  18
-# 6  21
-
-# graph styles can be modified by a second parameter to the plot method:
-# g.plot(graph.data.file("minimal.dat", x=1, y=2), [graph.style.line()])
