d0ac4855192d68daf3848235c27fd429076ff6cd
Joerg Lehmann <joerg@pyx-project.org>
2006-05-21 19:02:55 +0000

    describe plotting of more than one data set and graph key

--- examples/graphs/change.py
+++ examples/graphs/change.py
@@ -1,10 +1,14 @@
 from pyx import *
 
-g = graph.graphxy(width=10,
+g = graph.graphxy(width=8,
                   x=graph.axis.linear(min=0, max=2),
-                  y=graph.axis.linear(min=0, max=2))
-g.plot([graph.data.function("x(y)=y**(2**(3-%i))" % i) for i in range(3)] +
-       [graph.data.function("y(x)=x**(2**%i)" % i) for i in range(4)],
+                  y=graph.axis.linear(min=0, max=2),
+                  key=graph.key.key(pos="br", dist=0.1))
+g.plot([graph.data.function("x(y)=y**(2**%i)" % i, title=r"$x = y^{%g}$" % (2**i)) 
+        for i in range(2, 0, -1)] +
+       [graph.data.function("x(y)=y", title=r"$x = y$")] +
+       [graph.data.function("y(x)=x**(2**%i)" % i, title=r"$y = x^{%g}$" % (2**i)) 
+        for i in range(1, 3)],
        [graph.style.line([color.palette.Rainbow])])
 g.writeEPSfile("change")
 g.writePDFfile("change")
