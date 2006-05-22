3835f4111ac5e4af7aaeb3e7039fb9a857251ac0
Andre Wobst <wobsta@pyx-project.org>
2006-05-22 14:53:59 +0000

    some adjustments to make the examples nice-looking again after the dash/rellength/linewidth adjustment

--- examples/drawing/arrow.py
+++ examples/drawing/arrow.py
@@ -1,7 +1,7 @@
 from pyx import *
 
 c = canvas.canvas()
-c.stroke(path.curve(0, 0, 0, 3, 2, 3, 3, 2),
+c.stroke(path.curve(0, 0, 0, 4, 2, 4, 3, 3),
          [style.linewidth.THICK, style.linestyle.dashed, color.rgb.blue,
           deco.earrow([deco.stroked([color.rgb.red, style.linejoin.round]),
                        deco.filled([color.rgb.green])], size=1)])
--- examples/drawing/style.py
+++ examples/drawing/style.py
@@ -2,7 +2,7 @@ from pyx import *
 
 c = canvas.canvas()
 c.stroke(path.line(0, 0, 4, 0),
-         [style.linewidth.THIck, style.linestyle.dashed, color.rgb.red])
+         [style.linewidth.THICK, style.linestyle.dashed, color.rgb.red])
 c.stroke(path.line(0, -1, 4, -1),
          [style.linewidth(0.2), style.linecap.round, color.rgb.green])
 c.fill(path.rect(0, -3, 4, 1), [color.rgb.blue])
