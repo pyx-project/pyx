0e3d675a75488841b9256e8cf8eb35f9a629c40c
Joerg Lehmann <joerg@pyx-project.org>
2006-05-22 20:31:37 +0000

    moving around of examples and documentation of linkedaxis example

--- examples/axis/link.py
+++ examples/axis/link.py
@@ -3,9 +3,8 @@ from pyx import *
 
 c = canvas.canvas()
 
-g1 = c.insert(graph.graphxy(width=8,
-                            x=graph.axis.linear(min=0, max=1)))
-g1.plot(graph.data.function("y(x)=2*exp(-30*x)-exp(-3*x)"))
+g1 = c.insert(graph.graphxy(width=8))
+g1.plot(graph.data.function("y(x)=2*exp(-30*x)-exp(-3*x)", min=0, max=1))
 
 g2 = c.insert(graph.graphxy(width=8, ypos=g1.height+0.5,
                             x=graph.axis.linkedaxis(g1.axes["x"])))
