e90cc4db46aabf9a55cc7c6d37e65c0de9ee7e5e
Joerg Lehmann <joerg@pyx-project.org>
2006-05-22 20:29:30 +0000

    prevent vertical lines at divergencies

--- examples/graphs/axis.py
+++ examples/graphs/axis.py
@@ -2,7 +2,7 @@ from pyx import *
 
 g = graph.graphxy(width=8,
                   x=graph.axis.log(min=1e-1, max=1e4),
-                  y=graph.axis.lin(min=-5, max=5))
-g.plot(graph.data.function("y(x)=tan(log(1/x))"))
+                  y=graph.axis.lin(max=5))
+g.plot(graph.data.function("y(x)=tan(log(1/x))**2"))
 g.writeEPSfile("axis")
 g.writePDFfile("axis")
