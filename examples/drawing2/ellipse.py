b455ff2ff3954c84b888fc875f00eb136568dcb1
Joerg Lehmann <joerg@pyx-project.org>
2006-05-22 08:38:26 +0000

    mark parameters as Python code

--- examples/drawing2/ellipse.py
+++ examples/drawing2/ellipse.py
@@ -4,13 +4,14 @@ c = canvas.canvas()
 circ = path.circle(0, 0, 1)
 
 # variant 1: use trafo as a deformer
-c.stroke(circ, [style.linewidth.THIck, 
-                trafo.scale(sx=2, sy=0.9), trafo.rotate(45), trafo.translate(1, 0)])
+#c.stroke(circ, [style.linewidth.THIck, 
+#                trafo.scale(sx=2, sy=0.9), trafo.rotate(45), trafo.translate(1, 0)])
 
 # variant 2: transform a subcanvas
 sc = canvas.canvas()
 sc.stroke(circ, [style.linewidth.THIck])
-c.insert(sc, [trafo.scale(sx=2, sy=0.9), trafo.rotate(45), trafo.translate(5, 0)])
+# c.insert(sc, [trafo.scale(sx=2, sy=0.9), trafo.rotate(45), trafo.translate(5, 0)])
+c.insert(sc, [trafo.translate(5, 0), trafo.rotate(90)])
 
 c.writeEPSfile("ellipse")
 c.writePDFfile("ellipse")
