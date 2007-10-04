1a4998d7a02c525e9951d89dd7947c55a56e6185
Andre Wobst <wobsta@pyx-project.org>
2007-10-04 07:51:02 +0000

    remove spurious file (reported by GLI)

--- examples/bargraphs/months.py
+++
@@ -1,4 +0,0 @@
-bap = graph.axis.painter.bar
-a = graph.axis.nestedbar(painter=bap(nameattrs=[trafo.rotate(45),
-                                                text.halign.right],
-                                     innerticklength=0.1))
