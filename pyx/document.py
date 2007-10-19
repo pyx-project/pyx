f4e6b3989fa106b001ffe270b195e38ce1e3994c
Joerg Lehmann <joerg@pyx-project.org>
2007-10-19 19:37:00 +0000

    import sys module

--- pyx/document.py
+++ pyx/document.py
@@ -20,7 +20,7 @@
 # along with PyX; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
-import cStringIO, warnings
+import cStringIO, sys, warnings
 import bbox, pswriter, pdfwriter, trafo, style, unit
 
 
