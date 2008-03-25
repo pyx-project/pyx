0693eea96a3075ba5707600ed84d5d896d1c55cc
Michael Schindler <m-schindler@users.sourceforge.net>
2008-03-25 17:25:55 +0000

    added graph.data.values to the points example

--- examples/graphs/points.py
+++ examples/graphs/points.py
@@ -1,9 +1,12 @@
-# contributed by Chris Spencer
+# suggested by Chris Spencer
 
 from pyx import *
 
 g = graph.graphxy(width=8)
-g.plot(graph.data.points(zip(range(10),range(10)), x=1, y=2))
+# either provide lists of the individual coordinates
+g.plot(graph.data.values(x=range(10), y=range(10)))
+# or provide one list containing the whole points
+g.plot(graph.data.points(zip(range(10), range(10)), x=1, y=2))
 g.writeEPSfile("points")
 g.writePDFfile("points")
 
