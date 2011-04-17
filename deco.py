21dc369b03bd44d523d463750721a177ae617415
Michael Schindler <m-schindler@users.sourceforge.net>
2011-04-17 15:12:20 +0000

    added a brace decorator

--- deco.py
+++ deco.py
@@ -26,7 +26,7 @@
 #   should we at least factor it out?
 
 import sys, math
-import attr, canvas, canvasitem, color, path, normpath, style, trafo, unit
+import attr, canvas, canvasitem, color, path, normpath, style, trafo, unit, deformer
 
 _marker = object()
 
@@ -592,7 +592,6 @@ class shownormpath(deco, attr.attr):
                 dp.ornaments.draw(path.circle_pt(x_pt, y_pt, r_pt), [filled])
 
 
-
 class linehatched(deco, attr.exclusiveattr, attr.clearclass):
     """draws a pattern with explicit lines
 
@@ -764,4 +763,288 @@ class colorgradient(deco, attr.attr):
             c.stroke(dp.path.split([params[n-1],params[n+1]])[1], attr.mergeattrs([colors[n]] + self.attrs))
         c.stroke(dp.path.split(params[-2])[0], attr.mergeattrs([colors[-1]] + self.attrs))
         dp.ornaments.insert(c)
+
+
+class brace(deco, attr.attr):
+    r"""draws a nicely curled brace
+
+    In most cases, the original line is not wanted use canvas.canvas.draw(..) for it
+
+    Geometrical parameters:
+
+                 inner /\ strokes
+          ____________/  \__________
+         /   bar            bar     \ outer
+        /                            \ strokes
+
+    totalheight  distance from the jaws to the middle cap
+    barthickness  thickness of the main bars
+    innerstrokesthickness  thickness of the two ending strokes
+    outerstrokesthickness  thickness of the inner strokes at the middle cap
+    innerstrokesrelheight  height of the inner/outer strokes, relative to the total height
+    outerstrokesrelheight  this determines the angle of the main bars!
+                           should be around 0.5
+    Note: if innerstrokesrelheight + outerstrokesrelheight == 1 then the main bars
+          will be aligned parallel to the connecting line between the endpoints
+    outerstrokesangle  angle of the two ending strokes
+    innerstrokesangle  angle between the inner strokes at the middle cap
+    slantstrokesangle  extra slanting of the inner/outer strokes
+    innerstrokessmoothness  smoothing parameter for the inner + outer strokes
+    outerstrokessmoothness  should be around 1 (allowed: [0,infty))
+    middlerelpos  position of the middle cap (0 == left, 1 == right)
+    """
+    # This code is experimental because it is unclear
+    # how the brace fits into the concepts of PyX
+    #
+    # Some thoughts:
+    # - a brace needs to be decoratable with text
+    #   it needs stroking and filling attributes
+    # - the brace is not really a box:
+    #   it has two "anchor" points that are important for aligning it to other things
+    #   and one "anchor" point (plus direction) for aligning other things
+    # - a brace is not a deformer:
+    #   it does not look at anything else than begin/endpoint of a path
+    # - a brace might be a connector (which is to be dissolved into the box concept later?)
+
+    def __init__(self, reverse=1, stretch=None, dist=None, fillattrs=[],
+        totalheight=12*unit.x_pt,
+        barthickness=0.5*unit.x_pt, innerstrokesthickness=0.25*unit.x_pt, outerstrokesthickness=0.25*unit.x_pt,
+        innerstrokesrelheight=0.6, outerstrokesrelheight=0.7,
+        innerstrokesangle=30, outerstrokesangle=25, slantstrokesangle=5,
+        innerstrokessmoothness=2.0, outerstrokessmoothness=2.5,
+        middlerelpos=0.5):
+        self.fillattrs = fillattrs
+        self.reverse = reverse
+        self.stretch = stretch
+        self.dist = dist
+        self.totalheight            = totalheight
+        self.barthickness           = barthickness
+        self.innerstrokesthickness  = innerstrokesthickness
+        self.outerstrokesthickness  = outerstrokesthickness
+        self.innerstrokesrelheight  = innerstrokesrelheight
+        self.outerstrokesrelheight  = outerstrokesrelheight
+        self.innerstrokesangle      = innerstrokesangle
+        self.outerstrokesangle      = outerstrokesangle
+        self.slantstrokesangle      = slantstrokesangle
+        self.innerstrokessmoothness = innerstrokessmoothness
+        self.outerstrokessmoothness = outerstrokessmoothness
+        self.middlerelpos           = middlerelpos
+
+    def __call__(self, **kwargs):
+        for name in ["reverse", "stretch", "dist", "fillattrs",
+            "totalheight", "barthickness", "innerstrokesthickness", "outerstrokesthickness",
+            "innerstrokesrelheight", "outerstrokesrelheight", "innerstrokesangle", "outerstrokesangle", "slantstrokesangle",
+            "innerstrokessmoothness", "outerstrokessmoothness", "middlerelpos"]:
+            if not kwargs.has_key(name):
+                kwargs[name] = self.__dict__[name]
+        return brace(**kwargs)
+
+    def _halfbracepath_pt(self, length_pt, height_pt, ilength_pt, olength_pt, # <<<
+    ithick_pt, othick_pt, bthick_pt, cos_iangle, sin_iangle, cos_oangle,
+    sin_oangle, cos_slangle, sin_slangle):
+
+        ismooth = self.innerstrokessmoothness
+        osmooth = self.outerstrokessmoothness
+
+        # these two parameters are not important enough to be seen outside
+        inner_cap_param = 1.5
+        outer_cap_param = 2.5
+        outerextracurved = 0.6 # in (0, 1]
+        # 1.0 will lead to F=G, the outer strokes will not be curved at their ends.
+        # The smaller, the more curvature
+
+        # build an orientation path (three straight lines)
+        #
+        #      \q1
+        #    /  \
+        #   /    \
+        # _/      \______________________________________q5
+        #         q2         q3              q4           \
+        #                                                  \
+        #                                                   \
+        #                                                    \q6
+        #
+        # get the points for that:
+        q1 = (0, height_pt - inner_cap_param * ithick_pt + 0.5*ithick_pt/sin_iangle)
+        q2 = (q1[0] + ilength_pt * sin_iangle,
+              q1[1] - ilength_pt * cos_iangle)
+        q6 = (length_pt, 0)
+        q5 = (q6[0] - olength_pt * sin_oangle,
+              q6[1] + olength_pt * cos_oangle)
+        bardir = (q5[0] - q2[0], q5[1] - q2[1])
+        bardirnorm = math.hypot(*bardir)
+        bardir = (bardir[0]/bardirnorm, bardir[1]/bardirnorm)
+        ismoothlength_pt = ilength_pt * ismooth
+        osmoothlength_pt = olength_pt * osmooth
+        if bardirnorm < ismoothlength_pt + osmoothlength_pt:
+            ismoothlength_pt = bardirnorm * ismoothlength_pt / (ismoothlength_pt + osmoothlength_pt)
+            osmoothlength_pt = bardirnorm * osmoothlength_pt / (ismoothlength_pt + osmoothlength_pt)
+        q3 = (q2[0] + ismoothlength_pt * bardir[0],
+              q2[1] + ismoothlength_pt * bardir[1])
+        q4 = (q5[0] - osmoothlength_pt * bardir[0],
+              q5[1] - osmoothlength_pt * bardir[1])
+
+        #
+        #    P _O
+        #   / | \A2
+        #  / A1\ \
+        #   /   \ B2C2________D2___________E2_______F2___G2
+        #        \______________________________________  \
+        #       B1,C1         D1           E1      F1  G1  \
+        #                                                \  \
+        #                                                 \  \H2
+        #                                                H1\_/I2
+        #                                                  I1
+        #
+        # the halfbraces meet in P and A1:
+        P = (0, height_pt)
+        A1 = (0, height_pt - inner_cap_param * ithick_pt)
+        # A2 is A1, shifted by the inner thickness
+        A2 = (A1[0] + ithick_pt * cos_iangle,
+              A1[1] + ithick_pt * sin_iangle)
+        s, t = deformer.intersection(P, A2, (cos_slangle, sin_slangle), (sin_iangle, -cos_iangle))
+        O = (P[0] + s * cos_slangle,
+             P[1] + s * sin_slangle)
+
+        # from D1 to E1 is the straight part of the brace
+        # also back from E2 to D1
+        D1 = (q3[0] + bthick_pt * bardir[1],
+              q3[1] - bthick_pt * bardir[0])
+        D2 = (q3[0] - bthick_pt * bardir[1],
+              q3[1] + bthick_pt * bardir[0])
+        E1 = (q4[0] + bthick_pt * bardir[1],
+              q4[1] - bthick_pt * bardir[0])
+        E2 = (q4[0] - bthick_pt * bardir[1],
+              q4[1] + bthick_pt * bardir[0])
+        # I1, I2 are the control points at the outer stroke
+        I1 = (q6[0] - 0.5 * othick_pt * cos_oangle,
+              q6[1] - 0.5 * othick_pt * sin_oangle)
+        I2 = (q6[0] + 0.5 * othick_pt * cos_oangle,
+              q6[1] + 0.5 * othick_pt * sin_oangle)
+        # get the control points for the curved parts of the brace
+        s, t = deformer.intersection(A1, D1, (sin_iangle, -cos_iangle), bardir)
+        B1 = (D1[0] + t * bardir[0],
+              D1[1] + t * bardir[1])
+        s, t = deformer.intersection(A2, D2, (sin_iangle, -cos_iangle), bardir)
+        B2 = (D2[0] + t * bardir[0],
+              D2[1] + t * bardir[1])
+        s, t = deformer.intersection(E1, I1, bardir, (-sin_oangle, cos_oangle))
+        G1 = (E1[0] + s * bardir[0],
+              E1[1] + s * bardir[1])
+        s, t = deformer.intersection(E2, I2, bardir, (-sin_oangle, cos_oangle))
+        G2 = (E2[0] + s * bardir[0],
+              E2[1] + s * bardir[1])
+        # at the inner strokes: use curvature zero at both ends
+        C1 = B1
+        C2 = B2
+        # at the outer strokes: use curvature zero only at the connection to
+        # the straight part
+        F1 = (outerextracurved * G1[0] + (1 - outerextracurved) * E1[0],
+              outerextracurved * G1[1] + (1 - outerextracurved) * E1[1])
+        F2 = (outerextracurved * G2[0] + (1 - outerextracurved) * E2[0],
+              outerextracurved * G2[1] + (1 - outerextracurved) * E2[1])
+        # the tip of the outer stroke, endpoints of the bezier curve
+        H1 = (I1[0] - outer_cap_param * othick_pt * sin_oangle,
+              I1[1] + outer_cap_param * othick_pt * cos_oangle)
+        H2 = (I2[0] - outer_cap_param * othick_pt * sin_oangle,
+              I2[1] + outer_cap_param * othick_pt * cos_oangle)
+
+        #for qq in [A1,B1,C1,D1,E1,F1,G1,H1,I1,
+        #           A2,B2,C2,D2,E2,F2,G2,H2,I2,
+        #           O,P
+        #           ]:
+        #    cc.fill(path.circle(qq[0], qq[1], 0.5), [color.rgb.green])
+
+        # now build the right halfbrace
+        bracepath = path.path(path.moveto_pt(*A1))
+        bracepath.append(path.curveto_pt(B1[0], B1[1], C1[0], C1[1], D1[0], D1[1]))
+        bracepath.append(path.lineto_pt(E1[0], E1[1]))
+        bracepath.append(path.curveto_pt(F1[0], F1[1], G1[0], G1[1], H1[0], H1[1]))
+        # the tip of the right halfbrace
+        bracepath.append(path.curveto_pt(I1[0], I1[1], I2[0], I2[1], H2[0], H2[1]))
+        # the rest of the right halfbrace
+        bracepath.append(path.curveto_pt(G2[0], G2[1], F2[0], F2[1], E2[0], E2[1]))
+        bracepath.append(path.lineto_pt(D2[0], D2[1]))
+        bracepath.append(path.curveto_pt(C2[0], C2[1], B2[0], B2[1], A2[0], A2[1]))
+        # the tip in the middle of the brace
+        bracepath.append(path.curveto_pt(O[0], O[1], O[0], O[1], P[0], P[1]))
+
+        return bracepath
+    # >>>
+
+    def _bracepath(self, x0_pt, y0_pt, x1_pt, y1_pt): # <<<
+        height_pt = unit.topt(self.totalheight)
+        totallength_pt = math.hypot(x1_pt - x0_pt, y1_pt - y0_pt)
+        leftlength_pt = self.middlerelpos * totallength_pt
+        rightlength_pt = totallength_pt - leftlength_pt
+        ithick_pt = unit.topt(self.innerstrokesthickness)
+        othick_pt = unit.topt(self.outerstrokesthickness)
+        bthick_pt = unit.topt(self.barthickness)
+
+        # create the left halfbrace with positive slanting
+        # because we will mirror this part
+        cos_iangle = math.cos(math.radians(0.5*self.innerstrokesangle - self.slantstrokesangle))
+        sin_iangle = math.sin(math.radians(0.5*self.innerstrokesangle - self.slantstrokesangle))
+        cos_oangle = math.cos(math.radians(self.outerstrokesangle - self.slantstrokesangle))
+        sin_oangle = math.sin(math.radians(self.outerstrokesangle - self.slantstrokesangle))
+        cos_slangle = math.cos(math.radians(-self.slantstrokesangle))
+        sin_slangle = math.sin(math.radians(-self.slantstrokesangle))
+        ilength_pt = self.innerstrokesrelheight * height_pt / cos_iangle
+        olength_pt = self.outerstrokesrelheight * height_pt / cos_oangle
+
+        bracepath = self._halfbracepath_pt(leftlength_pt, height_pt,
+          ilength_pt, olength_pt, ithick_pt, othick_pt, bthick_pt, cos_iangle,
+          sin_iangle, cos_oangle, sin_oangle, cos_slangle,
+          sin_slangle).reversed().transformed(trafo.mirror(90))
+
+        # create the right halfbrace with negative slanting
+        cos_iangle = math.cos(math.radians(0.5*self.innerstrokesangle + self.slantstrokesangle))
+        sin_iangle = math.sin(math.radians(0.5*self.innerstrokesangle + self.slantstrokesangle))
+        cos_oangle = math.cos(math.radians(self.outerstrokesangle + self.slantstrokesangle))
+        sin_oangle = math.sin(math.radians(self.outerstrokesangle + self.slantstrokesangle))
+        cos_slangle = math.cos(math.radians(-self.slantstrokesangle))
+        sin_slangle = math.sin(math.radians(-self.slantstrokesangle))
+        ilength_pt = self.innerstrokesrelheight * height_pt / cos_iangle
+        olength_pt = self.outerstrokesrelheight * height_pt / cos_oangle
+
+        bracepath = bracepath << self._halfbracepath_pt(rightlength_pt, height_pt,
+        ilength_pt, olength_pt, ithick_pt, othick_pt, bthick_pt, cos_iangle,
+        sin_iangle, cos_oangle, sin_oangle, cos_slangle,
+        sin_slangle)
+
+        return bracepath.transformed(
+          # two trafos for matching the given endpoints
+          trafo.translate_pt(x0_pt, y0_pt) *
+          trafo.rotate_pt(math.degrees(math.atan2(y1_pt-y0_pt, x1_pt-x0_pt))) *
+          # one trafo to move the brace's left outer stroke to zero
+          trafo.translate_pt(leftlength_pt, 0))
+    # >>>
+
+    def decorate(self, dp, texrunner):
+        dp.ensurenormpath()
+        x0_pt, y0_pt = dp.path.atbegin_pt()
+        x1_pt, y1_pt = dp.path.atend_pt()
+        if self.reverse:
+            x0_pt, y0_pt, x1_pt, y1_pt = x1_pt, y1_pt, x0_pt, y0_pt
+        if self.stretch is not None:
+            xm, ym = 0.5*(x0_pt+x1_pt), 0.5*(y0_pt+y1_pt)
+            x0_pt, y0_pt = xm + self.stretch*(x0_pt-xm), ym + self.stretch*(y0_pt-ym)
+            x1_pt, y1_pt = xm + self.stretch*(x1_pt-xm), ym + self.stretch*(y1_pt-ym)
+        if self.dist is not None:
+            d = unit.topt(self.dist)
+            dx, dy = dp.path.rotation_pt(dp.path.begin()).apply_pt(0, 1)
+            x0_pt += d*dx; y0_pt += d*dy
+            dx, dy = dp.path.rotation_pt(dp.path.end()).apply_pt(0, 1)
+            x1_pt += d*dx; y1_pt += d*dy
+        dp.ornaments.fill(self._bracepath(x0_pt, y0_pt, x1_pt, y1_pt), self.fillattrs)
+
+brace.clear = attr.clearclass(brace)
+
+leftbrace  = brace(reverse=0, middlerelpos=0.55, innerstrokesrelheight=0.6, outerstrokesrelheight=0.7, slantstrokesangle=-10)
+rightbrace = brace(reverse=1, middlerelpos=0.45, innerstrokesrelheight=0.6, outerstrokesrelheight=0.7, slantstrokesangle=10)
+belowbrace = brace(reverse=1, middlerelpos=0.55, innerstrokesrelheight=0.7, outerstrokesrelheight=0.9, slantstrokesangle=-10)
+abovebrace = brace(reverse=0, middlerelpos=0.45, innerstrokesrelheight=0.7, outerstrokesrelheight=0.9, slantstrokesangle=-10)
+straightbrace = brace(innerstrokesrelheight=0.5, outerstrokesrelheight=0.5,
+        innerstrokesangle=30, outerstrokesangle=30, slantstrokesangle=0,
+        innerstrokessmoothness=1.0, outerstrokessmoothness=1.0)
 
