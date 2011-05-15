9415f872d574120ff1ebbfa01535b2b04acd37b0
Joerg Lehmann <joerg@pyx-project.org>
2011-05-15 15:05:42 +0000

    make use of setuptools configurable in setup.cfg

--- setup.py
+++ setup.py
@@ -8,16 +8,26 @@ combines an abstraction of the PostScript drawing model with a TeX/LaTeX
 interface. Complex tasks like 2d and 3d plots in publication-ready quality are
 built out of these primitives."""
 
-
-try:
-    from setuptools import setup, Extension
-    setuptools_args={"zip_safe": True}
-except ImportError:
-    from distutils.core import setup, Extension
-    setuptools_args={}
-
 import ConfigParser
 import pyx.version
+
+setuptools_args={}
+
+# obtain information on which modules have to be built and whether to use setuptools
+# instead of distutils from setup.cfg file
+cfg = ConfigParser.ConfigParser()
+cfg.read("setup.cfg")
+if cfg.has_section("PyX"):
+    if cfg.has_option("PyX", "build_pykpathsea") and cfg.getboolean("PyX", "build_pykpathsea"):
+        ext_modules.append(pykpathsea_ext_module)
+    if cfg.has_option("PyX", "build_t1code") and cfg.getboolean("PyX", "build_t1code"):
+        ext_modules.append(t1code_ext_module)
+    if cfg.has_option("PyX", "use_setuptools") and cfg.getboolean("PyX", "use_setuptools"):
+        from setuptools import setup, Extension
+        setuptools_args={"zip_safe": True}
+    else:
+        from distutils.core import setup, Extension
+
 
 #
 # build list of extension modules
@@ -30,14 +40,6 @@ pykpathsea_ext_module = Extension("pyx.pykpathsea",
 t1code_ext_module = Extension("pyx.font._t1code",
                               sources=["pyx/font/_t1code.c"])
 
-# obtain information on which modules have to be built from setup.cfg file
-cfg = ConfigParser.ConfigParser()
-cfg.read("setup.cfg")
-if cfg.has_section("PyX"):
-    if cfg.has_option("PyX", "build_pykpathsea") and cfg.getboolean("PyX", "build_pykpathsea"):
-        ext_modules.append(pykpathsea_ext_module)
-    if cfg.has_option("PyX", "build_t1code") and cfg.getboolean("PyX", "build_t1code"):
-        ext_modules.append(t1code_ext_module)
 
 description, long_description = __doc__.split("\n\n", 1)
 
