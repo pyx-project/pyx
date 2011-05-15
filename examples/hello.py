e3f7745d39d9d297563033c982ace9ade8287613
Joerg Lehmann <joerg@pyx-project.org>
2011-05-15 09:49:13 +0000

    revert change r3054 to use auto-guessing of output filename as intended (and mentioned in the description of the example)

--- examples/hello.py
+++ examples/hello.py
@@ -3,5 +3,5 @@ from pyx import *
 c = canvas.canvas()
 c.text(0, 0, "Hello, world!")
 c.stroke(path.line(0, 0, 2, 0))
-c.writeEPSfile("hello")
-c.writePDFfile("hello")
+c.writeEPSfile()
+c.writePDFfile()
