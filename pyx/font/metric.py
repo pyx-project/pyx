c70c7d8d16962630cb41a3d069642db725eca797
Andre Wobst <wobsta@pyx-project.org>
2011-07-07 21:16:51 +0000

    be more precise regarding the scale (add a missing _pt)

--- pyx/font/metric.py
+++ pyx/font/metric.py
@@ -26,19 +26,19 @@ class metric:
     def width_ds(self, glyphname):
         raise NotImplementedError()
 
-    def width_pt(self, glyphnames, size):
+    def width_pt(self, glyphnames, size_pt):
         raise NotImplementedError()
 
-    def height_pt(self, glyphnames, size):
+    def height_pt(self, glyphnames, size_pt):
         raise NotImplementedError()
 
-    def depth_pt(self, glyphnames, size):
+    def depth_pt(self, glyphnames, size_pt):
         raise NotImplementedError()
 
     def resolveligatures(self, glyphnames):
         return glyphnames
 
-    def resolvekernings(self, glyphnames, size=None):
+    def resolvekernings(self, glyphnames, size_pt=None):
         result = [None]*(2*len(glyphnames)-1)
         for i, glyphname in enumerate(glyphnames):
             result[2*i] = glyphname
