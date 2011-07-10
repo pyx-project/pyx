0f1a48c56b3357f96404e597923a4a0022c9503c
Andre Wobst <wobsta@pyx-project.org>
2011-07-10 10:04:24 +0000

    enable todo extension

--- manual/conf.py
+++ manual/conf.py
@@ -27,7 +27,7 @@ import pyx.version
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['mathjax']
+extensions = ['mathjax', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -220,3 +220,4 @@ man_pages = [
 # -- Other options  ------------------------------------------------------------
 
 mathjax_path = 'http://mathjax.connectmv.com/MathJax.js'
+todo_include_todos = True
