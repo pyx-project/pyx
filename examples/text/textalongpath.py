4422330f00b2c14d21883e4d65564a39226e52f2
Andre Wobst <wobsta@pyx-project.org>
2011-10-11 07:35:25 +0000

    improve the example to show more typical use cases

--- examples/text/textalongpath.py
+++ examples/text/textalongpath.py
@@ -3,10 +3,10 @@ from pyx import *
 c = canvas.canvas()
 
 p = path.path(path.moveto(-4, 0), path.curveto(-2, 0, -2, 1, 0, 1), path.curveto(2, 1, 2, 0, 4, 0))
-label = " ".join([r"\PyX{} is fun."]*3)
 
-c.draw(p, [trafo.translate(0, 1), deco.stroked([color.rgb.blue]), deco.curvedtext(label)])
-c.draw(p, [deco.stroked([color.rgb.red]), deco.curvedtext(label, textattrs=[text.vshift.mathaxis], exclude=0.2)])
+c.stroke(p, [trafo.translate(0, 1), deco.curvedtext(" ".join([r"\PyX{} is fun."]*3))])
+c.stroke(p, [deco.curvedtext("left", textattrs=[text.halign.left, text.vshift.mathaxis], arclenfrombegin=1, exclude=0.2),
+             deco.curvedtext("right", textattrs=[text.halign.right, text.vshift.mathaxis], arclenfromend=1, exclude=0.2)])
 
 c.writeEPSfile("textalongpath")
 c.writePDFfile("textalongpath")
