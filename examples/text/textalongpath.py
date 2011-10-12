eef6bcdc311264ec8e73f723b1b7ceb9f833eeae
Andre Wobst <wobsta@pyx-project.org>
2011-10-12 00:02:56 +0000

    do not clutter the example by using several paths

--- examples/text/textalongpath.py
+++ examples/text/textalongpath.py
@@ -2,11 +2,11 @@ from pyx import *
 
 c = canvas.canvas()
 
-p = path.path(path.moveto(-4, 0), path.curveto(-2, 0, -2, 1, 0, 1), path.curveto(2, 1, 2, 0, 4, 0))
+p = path.path(path.moveto(-2, 0), path.curveto(-1, 0, -1, 1, 0, 1), path.curveto(1, 1, 1, 0, 2, 0))
 
-c.stroke(p, [trafo.translate(0, 1), deco.curvedtext(" ".join([r"\PyX{} is fun."]*3))])
-c.stroke(p, [deco.curvedtext("left", textattrs=[text.halign.left, text.vshift.mathaxis], arclenfrombegin=1, exclude=0.2),
-             deco.curvedtext("right", textattrs=[text.halign.right, text.vshift.mathaxis], arclenfromend=1, exclude=0.2)])
+c.stroke(p, [deco.curvedtext("\PyX{} is fun!"),
+             deco.curvedtext("left", textattrs=[text.halign.left, text.vshift.mathaxis], arclenfrombegin=0.5, exclude=0.1),
+             deco.curvedtext("right", textattrs=[text.halign.right, text.vshift.mathaxis], arclenfromend=0.5, exclude=0.1)])
 
 c.writeEPSfile("textalongpath")
 c.writePDFfile("textalongpath")
