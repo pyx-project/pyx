0787e813c393f3324bd1f5f62123e44061607ebf
Andre Wobst <wobsta@pyx-project.org>
2012-10-17 10:40:28 +0000

    add html version of the faq

--- manual/conf.py
+++ manual/conf.py
@@ -27,7 +27,7 @@ import pyx.version
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['mathjax', 'sphinx.ext.todo']
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
