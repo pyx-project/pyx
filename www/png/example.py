3636c0159f4048ee09f3e2faf159b90da2e92a2a
Andre Wobst <wobsta@pyx-project.org>
2012-10-17 08:57:49 +0000

    fix example (disable new 3dgraph features)

--- www/png/example.py
+++ www/png/example.py
@@ -1,13 +1,6 @@
 import math
 from pyx import *
 
-class rgbgradient(color.lineargradient):
-
-    def getcolor(self, x):
-        return color.lineargradient.getcolor(self, x).rgb()
-
-rgbHue = rgbgradient(color.hsb(0, 1, 1), color.hsb(1, 1, 1))
-
 class phasecolorsurface(graph.style.surface):
 
     def midcolor(self, *c):
@@ -18,5 +11,5 @@ g = graph.graphxyz(size=3.5, zscale=0.3, projector=graph.graphxyz.central(3, 25,
                    y=graph.axis.lin(painter=None),
                    z=graph.axis.lin(painter=None),
                    x2=None, y2=None)
-g.plot(graph.data.file("example.dat", x=1, y=2, z=3, color=4), [phasecolorsurface(gradient=rgbHue, backcolor=color.rgb.black)])
+g.plot(graph.data.file("example.dat", x=1, y=2, z=3, color=4), [phasecolorsurface(gradient=color.rgbgradient.Rainbow, coloraxis=graph.axis.lin(min=-math.pi, max=math.pi), backcolor=color.rgb.black, keygraph=None)])
 g.writeEPSfile("example")
