f73b2ed75be9ec1e655a7f956d75b9f289f9e5cf
Andre Wobst <wobsta@pyx-project.org>
2013-08-02 23:41:28 +0000

    add a pyxinfo feature for debugging, help desk, etc.

--- __init__.py
+++ __init__.py
@@ -40,3 +40,24 @@ import importlib
 # automatically import main modules into pyx namespace
 for module in __all__:
     importlib.import_module('.' + module, package='pyx')
+
+def pyxinfo(show_files=True, show_executes=True):
+    import os, sys
+    from . import config
+    print("--- PyX info", "-"*37)
+    print("Platform name is:", os.name)
+    print("Python executable:", sys.executable)
+    print("Python version:", sys.version)
+    print("PyX comes from:", __file__)
+    print("PyX version:", __version__)
+    print("pyxrc", "is" if os.path.isfile(config.user_pyxrc) else "would be" ,"loaded from:", config.user_pyxrc)
+    print("pykpathsea:", "available" if config.has_pykpathsea else "not available")
+    print("file locators in use:", ", ".join(method.__class__.__name__ for method in config.methods))
+    print("show files:", show_files)
+    print("show executes:", show_executes)
+    print("-"*50)
+    config.show_files = show_files
+    config.show_executes = show_executes
+
+__all__.append("__version__")
+__all__.append("pyxinfo")
