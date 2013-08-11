11a8444e15b5351cd3edeac62730de77887f3a00
Andre Wobst <wobsta@pyx-project.org>
2013-08-11 15:55:54 +0000

    add ipynb files to examples

---
+++ examples/mkipynb.py
@@ -0,0 +1,26 @@
+import base64, os, re, sys
+from IPython.nbformat.current import reads, write, new_output
+
+filename = os.path.splitext(sys.argv[1])[0]
+
+title, description = open("{}.txt".format(filename), encoding="utf-8").read().split('\n\n', 1)
+description = description.replace("...", "").replace("'''", "**").replace("''", "*")
+bendpattern = re.compile("^!+", re.MULTILINE)
+bendcode = "![bend](http://pyx.sourceforge.net/bend.png)"
+description = re.sub(bendpattern, lambda m: "![bend](http://pyx.sourceforge.net/bend.png)"*(m.end()-m.start()), description)
+code = open("{}.py".format(filename), encoding="utf-8").read()
+code = re.sub('\.writeEPSfile\(("[a-z]+")?\)\n.*writePDFfile\(("[a-z]+")?\)', "", code)
+input = """# <headingcell level=1>
+{title}
+# <codecell>
+{code}
+# <markdowncell>
+{description}
+# <codecell>
+""".format(title=title, code=code, description=description)
+
+nb = reads(input, "py")
+codecell, = [cell for ws in nb.worksheets for cell in ws.cells if cell.cell_type == "code"]
+codecell.outputs.append(new_output(output_type="pyout",
+                                   output_png=base64.encodebytes(open("{}.png".format(filename), "rb").read())))
+write(nb, open("{}.ipynb".format(filename), "w"), "ipynb")
