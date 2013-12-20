4b0f7bb04be3c894fb4fc98e5ad2f7fa81f58ac8
Andre Wobst <wobsta@pyx-project.org>
2013-12-20 15:23:26 +0000

    don't add the PyX logo

--- faq/conf.py
+++ faq/conf.py
@@ -112,7 +112,7 @@ html_title = 'PyX FAQ'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../www/png/pyxlogo.png'
+#html_logo = None
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
