26c45767514413de4a21a443912a8a44b8682f82
Jörg Lehmann <joerg@pyx-project.org>
2023-09-23 16:26:26 +0200

    Move to pyproject.toml
    Move metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-"""Python package for the generation of PostScript, PDF, and SVG files
-
-PyX is a Python package for the creation of PostScript, PDF, and SVG files. It
-combines an abstraction of the PostScript drawing model with a TeX/LaTeX
-interface. Complex tasks like 2d and 3d plots in publication-ready quality are
-built out of these primitives."""
-
-import sys
-
-if sys.version_info[0] == 2:
-    print("""*** Sorry, this version of PyX runs on Python 3 only. ***
-If you want to use PyX on Python 2, please use one of our
-old releases up to PyX 0.12.x, i.e. execute something like:
-
-   pip install pyx==0.12.1
-""")
-    exit()
-
-
 from setuptools import setup, Extension
 import configparser
-import pyx.version
 
 cfg = configparser.ConfigParser()
 cfg.read("setup.cfg")
@@ -40,35 +20,4 @@ if cfg.has_option("PyX", "build_pykpathsea") and cfg.getboolean("PyX", "build_py
 if cfg.has_option("PyX", "build_t1code") and cfg.getboolean("PyX", "build_t1code"):
     ext_modules.append(t1code_ext_module)
 
-
-description, long_description = __doc__.split("\n\n", 1)
-
-setup(name="PyX",
-      version=pyx.version.version,
-      author="Jörg Lehmann, André Wobst",
-      author_email="pyx-devel@lists.sourceforge.net",
-      url="https://pyx-project.org/",
-      description=description,
-      long_description=long_description,
-      license="GPL",
-      packages=["pyx", "pyx/graph", "pyx/graph/axis", "pyx/font", "pyx/dvi", "pyx/metapost"],
-      package_data={"pyx": ["data/afm/*", "data/lfs/*", "data/def/*", "data/pyxrc"]},
-      ext_modules=ext_modules,
-      python_requires='>=3',
-      classifiers=["Development Status :: 3 - Alpha",
-                   "Intended Audience :: Developers",
-                   "Intended Audience :: End Users/Desktop",
-                   "License :: OSI Approved :: GNU General Public License (GPL)",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python :: 3",
-                   "Topic :: Multimedia :: Graphics",
-                   "Topic :: Scientific/Engineering :: Visualization",
-                   "Topic :: Software Development :: Libraries :: Python Modules"],
-      platforms="OS independent",
-      extras_require = {"dev": ["zope.pagetemplate",
-                                "sphinx",
-                                "sphinx_selective_exclude",
-                                "pillow",
-                                "IPython",
-                                "nbformat",
-                                "testfixtures"]})
+setup(ext_modules=ext_modules)
