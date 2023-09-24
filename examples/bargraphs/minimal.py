17bffdca78c1e38c9bb0b0d36b185f6828a26fc0
Jörg Lehmann <joerg@pyx-project.org>
2023-09-24 11:02:53 +0200

    Use standard idiom for output in bargraphs.minimal

--- examples/bargraphs/minimal.py
+++ examples/bargraphs/minimal.py
@@ -2,6 +2,6 @@ from pyx import *
 
 g = graph.graphxy(width=8, x=graph.axis.bar())
 g.plot(graph.data.file("minimal.dat", xname=0, y=2), [graph.style.bar()])
-g.writeEPSfile("minimal")
-g.writePDFfile("minimal")
-g.writeSVGfile("minimal")
+g.writeEPSfile()
+g.writePDFfile()
+g.writeSVGfile()
