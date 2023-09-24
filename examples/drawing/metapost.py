698b2307ca9d95f244f431d9cdcfaeb2837de6d0
Jörg Lehmann <joerg@pyx-project.org>
2023-09-24 09:53:44 +0200

    Simplify passing of points in example

--- examples/drawing/metapost.py
+++ examples/drawing/metapost.py
@@ -12,8 +12,8 @@ closedpath = metapost.path.path([
     smoothknot(*p5), tensioncurve()])
 c = canvas.canvas()
 for p in [p1, p2, p3, p4, p5]:
-    c.fill(path.circle(p[0], p[1], 0.05), [color.rgb.red])
-    c.fill(path.circle(p[0], p[1], 0.05), [color.rgb.red, trafo.translate(2, 0)])
+    c.fill(path.circle(*p, 0.05), [color.rgb.red])
+    c.fill(path.circle(*p, 0.05), [color.rgb.red, trafo.translate(2, 0)])
 c.stroke(openpath)
 c.stroke(closedpath, [trafo.translate(2, 0)])
 
